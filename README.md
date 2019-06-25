f782455f27d8e5a49849bc1189fbd6c3a98e7b31
Jhosef Marks de Carvalho <jhosef.carvalho@JVM60101279.local>
2019-06-25 15:39:04 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,98 @@
+# Introdução
+
+Repositório dos arquivos de estilo do PO Core.
+
+Esse repositório é usado para atualizar o CDN PO Core que deve ser utilizado como base por todos os projetos front-end da Portinari.
+
+# Processo de design
+
+Antes de desenvolver um novo componente, verifique se o protótipo tem a especificação para os seguintes status:
+
+- Enable
+- Disable
+- Static
+- Hover
+- Focus
+- Active
+
+> Quando um componente não tem um destes estados, ele **não deve ser desenvolvido**.
+
+# Como iniciar
+
+1. Instale as dependências
+`npm install` ou `yarn`
+
+2. Fica observando alterações no projeto e recria o build do projeto em tempo de desenvolvimento
+`npm run watch` ou `yarn watch`
+
+3. Inicia o servidor para testar o projeto
+`npm run dev` ou `yarn dev`
+
+Outros scripts:
+* Faz o build do projeto
+`npm run build` ou `yarn build`
+
+# Contribuição
+
+Siga as `convenções de nomenclatura` abaixo:
+
+O objetivo principal é continuar envolvendo os componentes, tornando-os mais simples de usar em qualquer projeto. O desenvolvimento do CDN 
+PO Core é aberto para todos os desenvolvedores da Portinari, e agradecemos aos desenvolvedores da Portinari que contribuem com melhorias e 
+correções de erros.
+
+Leia abaixo para saber como você pode participar na melhoria do CDN PO Core.
+
+### Steps
+
+1. Crie um template HTML com o nome do componente em `src/css/components`
+>2. Crie um link em `index.html` para redirecionar para o preview do componente
+3. Crie uma pasta com o nome do componente em `src/css`
+4. Crie um arquivo com o nome do componente e o sufixo `-map.css`
+5. Crie uma `const` para referenciar o componente em `main.js`
+6. Crie uma pasta `css` dentro da pasta que você criou com o nome do componente
+7. Crie um arquivo `css` com o nome do componente
+8. Execute a tarefa `gulp inject` para injetar o `<link href="dist/styles.css" />` localmente
+9. Quando fazer o Pull Request, por favor rode a tarefa `gulp remove-inject` para referenciar o CDN styles
+
+### Exemplo de contribuição
+
+Nós queremos criar o componente `po-menu`.
+1. O nome do arquivo deve ser: `src/templates/menu.html`
+2. `<a class="col-12 po-link" href="@PATH/src/templates/menu.html">po-Menu</a>`
+3. O nome da pasta deve ser:`src/css/menu`
+4. O nome do arquivo deve ser: `src/css/menu/menu-map.css`
+5. Criar uma constante dentro do arquivo `main.js`: `const menu = require('./src/css/menu/menu-map.css');`
+6. O nome da pasta deve ser: `src/css/menu/css`
+7. O nome do arquivo deve ser: `src/css/menu/css/menu.css`
+
+### Dica para contribuir
+
+* Dentro do arquivo `[<component>]-map.css` você deve usar `@import` dos arquivos CSS.
+  * Exemplo: `@import 'css/menu.css'`
+
+# Convenções de nomenclatura
+
+### Utilitários
+
+Classes que podem ser aplicadas para os componentes e outros elementos.
+
+| Sintaxe                      | Exemplo                |
+| -----------------------------|------------------------|
+| `.po-u-[<utility>]`         | `.po-u-margin-top-10` |
+
+### Componentes
+
+| Sintax                                        | Exemplo                    |
+| ----------------------------------------------|----------------------------|
+| `.po-[<component>]`                          | `.po-menu`                |
+| `.po-[<component>][-descendent]`             | `.po-menu-header`         |
+| `.po-[<component>][-descendent][--modifier]` | `.po-menu-header--primary`|
+| `.po-[<component>].is[-state]`               | `.po-menu.is-disable`     |
+
+### Assets
+
+Instalar [SVGO](https://github.com/svg/svgo) para otimizar arquivos SVG.
+
+`$ [sudo] npm install -g svgo`
+
+> **Arquivos SVG precisam ser otimizados antes de serem adicionados ao projeto**
---
+++ gulpfile.js
@@ -0,0 +1,189 @@
+const { dest, parallel, series, src, watch } = require('gulp');
+
+const argv = require('yargs').argv;
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const del = require('del');
+const version = require('./package.json').version;
+
+const concat = require('gulp-concat');
+const minify = require('gulp-minify');
+const postcss = require('gulp-postcss');
+const rename = require('gulp-rename');
+const tap = require('gulp-tap');
+
+const apply = require('postcss-apply');
+const customProperties = require('postcss-custom-properties')
+const importCss = require('postcss-import');
+const nested = require('postcss-nested');
+
+const capitalize = string =>
+  string.split('-')
+    .map(part => part.charAt(0).toUpperCase() + part.substring(1).toLowerCase())
+    .join(' ');
+
+/**
+ * ============================================================
+ * TAREFAS PARA A GERAÇÃO DOS PACKAGES DOS TEMAS
+ * ============================================================
+ */
+const cleanTemp = () => del('./.temp');
+const cleanThemeDir = () => del('./dist');
+
+const copyThemeAssets = () =>
+  src('./src/assets/**/*.*').pipe(dest(`./dist/po-theme${argv.theme ? '-' + argv.theme : ''}/`));
+
+const copyThemePackageJson = () =>
+  src('package.json')
+    .pipe(tap(file => {
+      const contents = JSON.parse(file.contents.toString());
+
+      delete contents.devDependencies;
+      delete contents.scripts;
+
+      contents.name = `@portinari/style${argv.theme ? '-' + argv.theme : ''}`;
+      contents.description = `Portinari - Theme${argv.theme ? ' ' + capitalize(argv.theme) : ''}`;
+
+      file.contents = new Buffer(JSON.stringify(contents, null, 2), 'utf-8');
+    }))
+    .pipe(dest(`./dist/po-theme${argv.theme ? '-' + argv.theme : ''}/`));
+
+const prepareThemeCss = () => src('./src/**/*.css').pipe(dest('./.temp'));
+
+const buildThemeCss = () =>
+  src('./.temp/css/index.css')
+    .pipe(tap(file => {
+      const contents = file.contents.toString().replace(/\${theme}/, (argv.theme || 'default'));
+
+      file.contents = new Buffer(contents, 'utf-8');
+    }))
+    .pipe(postcss([
+      importCss(),
+      apply(),
+      nested(),
+      customProperties({
+        preserve: false,
+        warnings: true
+      }),
+      autoprefixer(),
+      cssnano()
+    ]))
+    .pipe(rename(`css/po-theme-default.min.css`))
+    .on('error', err => {
+      console.log(err.toString());
+      this.emit('end');
+    })
+    .pipe(dest(`./dist/po-theme${argv.theme ? '-' + argv.theme : ''}/`));
+
+// Tarefa otimizada para desenvolvimento
+const buildDevThemeCss = () =>
+  src('./.temp/css/index.css')
+    .pipe(tap(file => {
+      const contents = file.contents.toString().replace(/\${theme}/, (argv.theme || 'default'));
+
+      file.contents = new Buffer(contents, 'utf-8');
+    }))
+    .pipe(postcss([
+      importCss(),
+      apply(),
+      nested()
+    ]))
+    .pipe(rename(`css/po-theme-default.min.css`))
+    .on('error', err => {
+      console.log(err.toString());
+      this.emit('end');
+    })
+    .pipe(dest(`./dist/po-theme${argv.theme ? '-' + argv.theme : ''}/`));
+
+const buildTheme = series(
+  cleanTemp,
+  parallel(copyThemeAssets, copyThemePackageJson, prepareThemeCss),
+  buildThemeCss,
+  cleanTemp
+);
+buildTheme.displayName = 'build';
+
+/**
+ * ============================================================
+ * TAREFAS PARA A GERAÇÃO DO APP DE TESTES DOS TEMAS
+ * ============================================================
+ */
+const cleanAppDir = () => del('./app-dist');
+
+const copyApp = () => src('./src/app/*.*').pipe(dest('./app-dist'));
+const copyAppAssets = () => src('./src/app/assets/**/*.*').pipe(dest('./app-dist/assets'));
+
+const copyAppComponents = () => src(['./src/css/**/*.html', './src/css/**/*.js']).pipe(dest('./app-dist/css'));
+
+const copyThemeToApp = () =>
+  src([`./dist/po-theme${argv.theme ? '-' + argv.theme : ''}/**/*.*`, '!./dist/*.json'])
+    .pipe(dest('./app-dist/assets/'));
+
+const buildAppJs = () =>
+  src(['./src/app/js/*.js', '!./src/app/js/po-chart.js'])
+    .pipe(concat('app.js'))
+    .pipe(minify({
+      ext: {
+        min: '.min.js'
+      },
+      noSource: true
+    }))
+    .pipe(tap(file => {
+      file.contents = new Buffer(`var version = '${version}';` + file.contents.toString(), 'utf-8');
+    }))
+    .pipe(dest('./app-dist/js'));
+
+const buildApp = series(
+  cleanAppDir,
+  parallel(copyApp, copyAppAssets, copyAppComponents, buildAppJs),
+  buildTheme,
+  copyThemeToApp
+);
+buildApp.displayName = 'build:app';
+
+/**
+ * ============================================================
+ * TAREFAS GENÉRICAS
+ * ============================================================
+ */
+const clean = parallel(cleanThemeDir, cleanAppDir, cleanTemp);
+
+const watchers = () => {
+  console.warn('\n');
+  console.warn('   ╔═════════════════════════════════════════════════╗');
+  console.warn('   ║                                                 ║');
+  console.warn('   ║   NÃO ESQUEÇA DE INICIAR O SEU SERVIDOR HTTP!   ║');
+  console.warn('   ║                                                 ║');
+  console.warn('   ║   Use o servidor que você achar melhor:         ║');
+  console.warn('   ║   - live-server                                 ║');
+  console.warn('   ║   - http-server                                 ║');
+  console.warn('   ║   - tanto faz ...                               ║');
+  console.warn('   ║                                                 ║');
+  console.warn('   ║   Ao atualizar um arquivo CSS ou HTML a sua     ║');
+  console.warn('   ║   aplicação será carregada automaticamente!     ║');
+  console.warn('   ║                                                 ║');
+  console.warn('   ╚═════════════════════════════════════════════════╝');
+
+  watch('./src/**/*.html', copyAppComponents);
+  watch('./src/**/*.css', series(prepareThemeCss, buildDevThemeCss, copyThemeToApp));
+}
+
+/**
+ * ============================================================
+ * EXPORT DAS TAREFAS DO GULP
+ * ============================================================
+ */
+// gulp clean
+exports.clean = clean;
+
+// gulp build
+exports.build = buildTheme;
+
+// gulp build:app
+exports.buildApp = buildApp;
+
+// gulp
+exports.default = series(clean, buildApp);
+
+// gulp watch
+exports.watch = series(clean, buildApp, watchers);
