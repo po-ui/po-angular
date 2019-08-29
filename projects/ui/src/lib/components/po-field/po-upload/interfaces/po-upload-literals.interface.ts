726d5408959a1fa404fced7cea436af9c34d4238
jhonyeduardo <jhonyeduardo96@gmail.com>
2019-08-29 15:42:11 -0300

    feat(upload): utiliza o componente progress como barra de progresso
    Atualiza a identidade visual da barra de progresso no componente upload e
    também utiliza a ideia de componentização
    
    Fixes DTHFUI-1587

--- projects/ui/src/lib/components/po-field/po-upload/interfaces/po-upload-literals.interface.ts
+++ projects/ui/src/lib/components/po-field/po-upload/interfaces/po-upload-literals.interface.ts
@@ -7,10 +7,24 @@
  */
 export interface PoUploadLiterals {
 
-  /** Texto exibido no label para cancelar o envio. */
+  /**
+   * @deprecated 3.X.X
+   * @description
+   *
+   * **Deprecated**
+   *
+   * Texto exibido no label para cancelar o envio.
+   */
   cancel?: string;
 
-  /** Texto exibido no label para excluir o arquivo. */
+  /**
+   * @deprecated 3.X.X
+   * @description
+   *
+   * **Deprecated**
+   *
+   * Texto exibido no label para excluir o arquivo.
+   */
   deleteFile?: string;
 
   /** Texto exibido na área onde podem ser arrastados os arquivos ao utilizar a opção de *dragDrop*. */
@@ -34,7 +48,20 @@ export interface PoUploadLiterals {
   /** Texto exibido no label do botão para iniciar o envio dos arquivos. */
   startSending?: string;
 
-  /** Texto exibido no label para tentar novamente. */
+  /**
+   * @deprecated 3.X.X
+   * @description
+   *
+   * **Deprecated**
+   *
+   * Texto exibido no label para tentar novamente.
+   */
   tryAgain?: string;
+
+  /** Texto a ser exibido quando ocorrer erro no envio do arquivo. */
+  errorOccurred?: string;
+
+  /** Texto a ser exibido quando o envio do arquivo for realizado com sucesso. */
+  sentWithSuccess?: string;
 
 }
