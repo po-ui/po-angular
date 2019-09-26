4b6d6fa12098c4d4c7d84942a6e9886d624e2eb0
mateusjmaf <mateus.jmaf@gmail.com>
2019-09-26 18:06:08 -0300

    feat(tag): permitir o uso da paleta de cores e ícones
    Agora o componente permite a utilização da paleta de cores
    além de ícones e navegação através do teclado.
    
    Fixes DTHFUI-1762

--- projects/ui/src/lib/components/po-tag/po-tag-base.component.spec.ts
+++ projects/ui/src/lib/components/po-tag/po-tag-base.component.spec.ts
@@ -1,12 +1,14 @@
 import { expectPropertiesValues } from '../../util-test/util-expect.spec';
 
+import { PoColorPaletteEnum } from '../../enums/po-color-palette.enum';
+
 import { PoTagBaseComponent } from './po-tag-base.component';
-import { PoTagIcon } from './enums/po-tag-icon.enum';
 import { PoTagOrientation } from './enums/po-tag-orientation.enum';
 import { PoTagType } from './enums/po-tag-type.enum';
 
 describe('PoTagBaseComponent:', () => {
   const component = new PoTagBaseComponent();
+  const poTagColors = (<any>Object).values(PoColorPaletteEnum);
 
   it('should be created', () => {
     expect(component instanceof PoTagBaseComponent).toBeTruthy();
@@ -14,16 +16,37 @@ describe('PoTagBaseComponent:', () => {
 
   describe('Properties:', () => {
 
-    it('icon: should update with true value.', () => {
+    it('color: should update to true value.', () => {
+      const colorsValidTrueValues = poTagColors;
+
+      expectPropertiesValues(component, 'color', colorsValidTrueValues, colorsValidTrueValues);
+    });
+
+    it('color: shouldn´t update to false value.', () => {
+      const colorsInalidTrueValues = [undefined, null, 2, 'string', 0, NaN];
+
+      expectPropertiesValues(component, 'color', colorsInalidTrueValues, undefined);
+    });
+
+    it('icon: should update to true value with `type`.', () => {
+      component.type = PoTagType.Info;
       const booleanValidTrueValues = [true, 'true', 1, ''];
 
       expectPropertiesValues(component, 'icon', booleanValidTrueValues, true);
     });
 
-    it('icon: should update with false value.', () => {
+    it('icon: should update to false if `type` is true.', () => {
+      component.type = PoTagType.Info;
       const booleanInvalidValues = [undefined, null, 2, 'string', 0, NaN];
 
       expectPropertiesValues(component, 'icon', booleanInvalidValues, false);
+    });
+
+    it('icon: should update to true if `type` is undefined.', () => {
+      component.type = undefined;
+      const validValues = ['po-icon-ok', 'po-icon-company', 'po-icon-portinari'];
+
+      expectPropertiesValues(component, 'icon', validValues, validValues);
     });
 
     it('orientation: should update property with valid values', () => {
@@ -47,21 +70,7 @@ describe('PoTagBaseComponent:', () => {
     it('type: should update property with `info` if values are invalid', () => {
       const invalidValues = [undefined, null, '', true, false, 0, -1, 12, 15, 'aa', [], {}];
 
-      expectPropertiesValues(component, 'type', invalidValues, 'info');
-    });
-
-    it('iconFromType: should update property with valid values', () => {
-      component.type = PoTagType.Danger;
-      expect(component.iconFromType).toBe(PoTagIcon.Danger);
-
-      component.type = PoTagType.Danger;
-      expect(component.iconFromType).toBe(PoTagIcon.Danger);
-
-      component.type = PoTagType.Danger;
-      expect(component.iconFromType).toBe(PoTagIcon.Danger);
-
-      component.type = PoTagType.Danger;
-      expect(component.iconFromType).toBe(PoTagIcon.Danger);
+      expectPropertiesValues(component, 'type', invalidValues, undefined);
     });
 
   });
--- projects/ui/src/lib/components/po-tag/samples/sample-po-tag-labs/sample-po-tag-labs.component.ts
+++ projects/ui/src/lib/components/po-tag/samples/sample-po-tag-labs/sample-po-tag-labs.component.ts
@@ -1,19 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 
-import { PoRadioGroupOption, PoTagOrientation, PoTagType } from '@portinari/portinari-ui';
+import { PoRadioGroupOption, PoSelectOption, PoTagOrientation, PoTagType } from '@portinari/portinari-ui';
 
 @Component({
   selector: 'sample-po-tag-labs',
   templateUrl: './sample-po-tag-labs.component.html',
+  styles: [`
+    .sample-tag-color-circle {
+      border-radius: 10px;
+      display: inline-block;
+      height: 16px;
+      margin-right: 4px;
+      vertical-align: middle;
+      width: 16px;
+    }
+  `]
 })
 export class SamplePoTagLabsComponent implements OnInit {
 
+  color: string;
   event: string;
-  icon: boolean;
+  icon: boolean | string;
   label: string;
   orientation: PoTagOrientation;
   type: PoTagType;
   value: string;
+
+  public readonly colorList: Array<PoSelectOption> = [
+    { label: 'color-01', value: 'color-01' },
+    { label: 'color-02', value: 'color-02' },
+    { label: 'color-03', value: 'color-03' },
+    { label: 'color-04', value: 'color-04' },
+    { label: 'color-05', value: 'color-05' },
+    { label: 'color-06', value: 'color-06' },
+    { label: 'color-07', value: 'color-07' },
+    { label: 'color-08', value: 'color-08' },
+    { label: 'color-09', value: 'color-09' },
+    { label: 'color-10', value: 'color-10' },
+    { label: 'color-11', value: 'color-11' },
+    { label: 'color-12', value: 'color-12' },
+  ];
+
+  public readonly iconList: Array<PoSelectOption> = [
+    { label: 'po-icon-bluetooth', value: 'po-icon-bluetooth' },
+    { label: 'po-icon-like', value: 'po-icon-like' },
+    { label: 'po-icon-light', value: 'po-icon-light' },
+    { label: 'po-icon-star', value: 'po-icon-star' },
+    { label: 'po-icon-settings', value: 'po-icon-settings' },
+    { label: 'po-icon-world', value: 'po-icon-world' }
+  ];
 
   public readonly orientationOptions: Array<PoRadioGroupOption> = [
     { label: 'Horizontal', value: PoTagOrientation.Horizontal },
@@ -21,6 +56,7 @@ export class SamplePoTagLabsComponent implements OnInit {
   ];
 
   public readonly typeOptions: Array<PoRadioGroupOption> = [
+    { label: 'None', value: undefined },
     { label: 'Info', value: PoTagType.Info },
     { label: 'Danger', value: PoTagType.Danger },
     { label: 'Success', value: PoTagType.Success },
@@ -36,6 +72,7 @@ export class SamplePoTagLabsComponent implements OnInit {
   }
 
   restore() {
+    this.color = undefined;
     this.icon = undefined;
     this.label = undefined;
     this.orientation = undefined;
