b9cf19b4779fe43d91e97f8899470f0ba2f8a520
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2019-09-27 19:46:50 -0300

    fix(page-dynamic-edit): corrige importação do DynamicForm
    O formulário dynamicForm não era carregado corretamente causando o seguinte erro
    ERROR TypeError: Cannot read property 'form' of undefined
    quando usuário selecinonava as opções de salvar ou salvar novo
    Alterada a importação do ViewChild utilizando a flag static como false
    Ver (https://angular.io/api/core/ViewChild)

---
+++ projects/templates/src/lib/components/po-page-dynamic-edit/test/po-dynamic-form-stub-component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PoDynamicFormComponent } from '@portinari/portinari-ui';
+
+@Component({
+  selector: 'po-dynamic-form',
+  template: '',
+  providers: [
+    {
+      provide: PoDynamicFormComponent,
+      useClass: PoDynamicFormStubComponent
+    }
+  ]
+})
+
+export class PoDynamicFormStubComponent {
+
+  private _form: NgForm;
+
+  get form(): NgForm {
+    return this._form;
+  }
+
+  set form(form: NgForm) {
+    this._form = form;
+  }
+}
