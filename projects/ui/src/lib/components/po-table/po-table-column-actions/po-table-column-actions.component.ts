b8750b02466d681035c040239a586d239754a9c4
Joao da Silva <joao.lennon@totvs.com.br>
2019-10-26 14:44:38 -0300

    feat(table): add component doc

--- projects/ui/src/lib/components/po-table/po-table-column-actions/po-table-column-actions.component.ts
+++ projects/ui/src/lib/components/po-table/po-table-column-actions/po-table-column-actions.component.ts
@@ -1,12 +1,20 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { PoTableColumnAction } from './po-table-column-action.interface'
 
+/**
+ * @docsPrivate
+ *
+ * @description
+ *
+ * Componente responsável por exibir ações para uma determinada linha na tabela.
+ */
 @Component({
   selector: 'po-table-column-actions',
   templateUrl: './po-table-column-actions.component.html'
 })
 export class PoTableColumnActionsComponent {
 
+  /** Ações que devem ser exibidas para uma determinada linha da tabela. */
   private _actions: Array<PoTableColumnAction>;
   @Input('p-actions') set actions(actions: Array<PoTableColumnAction>) {
     this._actions = actions ? actions : new Array<PoTableColumnAction>();
