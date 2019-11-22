5f1a42449672cf58aeabe9ff267b1f987ebdb180
Samir Hassan Ayoub <123samir@gmail.com>
2019-11-22 11:37:34 -0300

    feat(combo): permite agrupamento dos itens
    Agora é possível agrupar os itens exibidos pelo componente
    viabilizando uma melhor visualização da lista de opções.
    
    Fixes DTHFUI-2192

---
+++ projects/ui/src/lib/components/po-field/po-combo/interfaces/po-combo-group.interface.ts
@@ -0,0 +1,11 @@
+import { PoComboOption } from './po-combo-option.interface';
+import { PoComboOptionGroup } from './po-combo-option-group.interface';
+
+/**
+ * @docsPrivate
+ *
+ * @description
+ *
+ * Interface que extende as propriedades existentes nas interfaces `PoComboOption` e `PoComboOptionGroup`.
+ */
+export interface PoComboGroup extends Partial<PoComboOption>, Partial <PoComboOptionGroup> { }
---
+++ projects/ui/src/lib/components/po-field/po-combo/interfaces/po-combo-option-group.interface.ts
@@ -0,0 +1,17 @@
+import { PoComboOption } from './po-combo-option.interface';
+
+/**
+ * @usedBy PoComboComponent
+ *
+ * @description
+ *
+ * Interface dos agrupamentos da coleção que será exibida no dropdown do `po-combo`.
+ */
+export interface PoComboOptionGroup {
+
+  /** Título para cada grupo de opções. */
+  label: string;
+
+  /** Lista de itens a serem exibidos. */
+  options: Array<PoComboOption>;
+}
