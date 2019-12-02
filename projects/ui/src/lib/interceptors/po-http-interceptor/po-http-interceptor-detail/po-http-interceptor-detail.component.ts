3e9a06d156df414e277e610b60524b16a3228bf2
Rafaelly <you@example.com>
2019-12-02 17:25:35 -0300

    feat(http-interceptor): permite exibição múltiplas mensagens do back end
    Exibe em uma modal os detalhamentos de mensagens de erro e sucesso
    recebidas do back end.
    
    Fixes DTHFUI-898

---
+++ projects/ui/src/lib/interceptors/po-http-interceptor/po-http-interceptor-detail/po-http-interceptor-detail.component.ts
@@ -0,0 +1,85 @@
+import { Component, EventEmitter, ViewChild } from '@angular/core';
+
+import { getShortBrowserLanguage } from '../../../utils/util';
+import { PoModalAction } from '../../../components/po-modal/po-modal-action.interface';
+import { PoModalComponent } from '../../../components/po-modal/po-modal.component';
+
+import { PoHttpInterceptorDetail } from './po-http-interceptor-detail.interface';
+import { poHttpInterceptorDetailLiteralsDefault } from './po-http-interceptor-detail-literals.interface';
+
+export const colors = { success: 'color-11', error: 'color-07', warning: 'color-08', info: '' };
+
+/**
+ * @docsPrivate
+ *
+ * @description
+ *
+ * Componente para a modal de detalhes exibida pelo interceptor
+ */
+@Component({
+  selector: 'po-http-interceptor-detail',
+  templateUrl: './po-http-interceptor-detail.component.html',
+})
+export class PoHttpInterceptorDetailComponent {
+
+  @ViewChild(PoModalComponent, { static: true }) modal: PoModalComponent;
+
+  closed = new EventEmitter<any>();
+  details: Array<PoHttpInterceptorDetail> = [];
+  language = getShortBrowserLanguage();
+  literals = poHttpInterceptorDetailLiteralsDefault[this.language];
+
+  primaryAction: PoModalAction = {
+    action: () => this.close(),
+    label: this.literals.closeButton
+  };
+
+  title: string;
+
+  set detail(details: Array<PoHttpInterceptorDetail>) {
+
+    if (details && details.length) {
+      this.details = this.filterByValidDetails(details);
+    }
+
+    this.title = this.formatTitle(this.details);
+  }
+
+  close() {
+    this.modal.close();
+    this.closed.emit();
+  }
+
+  formatDetailItemTitle(detail) {
+    return detail.code ? `${ detail.code } - ${ detail.message }` : detail.message;
+  }
+
+  open() {
+    this.modal.open();
+  }
+
+  typeColor(type: string): string {
+    return colors[type];
+  }
+
+  typeValue(type: string): string {
+    return poHttpInterceptorDetailLiteralsDefault[this.language][type] || type;
+  }
+
+  private addValidDetail(newDetails: Array<PoHttpInterceptorDetail>, detail: PoHttpInterceptorDetail) {
+    return detail.message ? newDetails.concat(this.getValidDetailProperties(detail)) : newDetails;
+  }
+
+  private getValidDetailProperties({ code, message, detailedMessage, type }: PoHttpInterceptorDetail) {
+    return { code, message, detailedMessage, type };
+  }
+
+  private filterByValidDetails(details: Array<PoHttpInterceptorDetail>) {
+    return details.reduce((newDetails, detail) => this.addValidDetail(newDetails, detail), []);
+  }
+
+  private formatTitle(details: Array<PoHttpInterceptorDetail>) {
+    return details.length > 1 ? `${this.literals.details} (${details.length})` : this.literals.detail;
+  }
+
+}
