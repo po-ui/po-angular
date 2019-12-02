879c715de74973772552eb9df27cf1e08d4183d7
alinelariguet <alinecbgmoreira@gmail.com>
2019-12-02 12:12:57 -0300

    feat(dynamic-view): permite executar uma função durante a inicialização
    Permite executar uma função ou fazer uma requisição durante a inicialização do componente,
    através da propriedade `p-load`, possibilitando alterar a visualização das informações.
    
    Fixes DTHFUI-2417

---
+++ projects/ui/src/lib/components/po-dynamic/po-dynamic-view/po-dynamic-view.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { PoDynamicFormField } from '../po-dynamic-form/po-dynamic-form-field.interface';
+
+@Injectable()
+export class PoDynamicViewService {
+
+  constructor(private http: HttpClient) { }
+
+  onLoad(url: string, value): Promise<{ value?: any, fields?: Array<PoDynamicFormField> }> {
+    return this.http.post(url, value).toPromise();
+  }
+
+}
