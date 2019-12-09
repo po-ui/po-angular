5912b348dcc0c0e95fc6b7dd33e86c3ebe36426d
Nicole Oliveira <nicoleoliveira1995@gmail.com>
2019-12-09 15:02:08 -0300

    feat(dynamic-form): remove a validação das propriedades

--- projects/ui/src/lib/components/po-dynamic/po-dynamic-form/po-dynamic-form-validation/po-dynamic-form-validation.service.ts
+++ projects/ui/src/lib/components/po-dynamic/po-dynamic-form/po-dynamic-form-validation/po-dynamic-form-validation.service.ts
@@ -2,50 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { of } from 'rxjs';
-import { map } from 'rxjs/operators';
-
-import { mapObjectByProperties } from '../../../../utils/util';
 import { PoDynamicFormField } from '../po-dynamic-form-field.interface';
 import { PoDynamicFormValidation } from './po-dynamic-form-validation.interface';
-
-const fieldProperties = [
-  'key',
-  'label',
-  'gridColumns',
-  'gridSmColumns',
-  'gridMdColumns',
-  'gridLgColumns',
-  'gridXlColumns',
-  'visible',
-  'divider',
-  'type',
-  'property',
-  'columns',
-  'required',
-  'options',
-  'optionsMulti',
-  'optionsService',
-  'searchService',
-  'mask',
-  'pattern',
-  'minLength',
-  'maxLength',
-  'disabled',
-  'help',
-  'booleanTrue',
-  'booleanFalse',
-  'maxValue',
-  'minValue',
-  'rows',
-  'secret'
-];
 
 @Injectable()
 export class PoDynamicFormValidationService {
 
   constructor(private http: HttpClient) { }
 
-    sendChanges(validate: Function | string, field: PoDynamicFormField, value: any) {
+  sendChanges(validate: Function | string, field: PoDynamicFormField, value: any) {
     const changedValue = { property: field.property, value };
 
     return typeof validate === 'string' ?
@@ -53,44 +18,26 @@ export class PoDynamicFormValidationService {
   }
 
   sendFieldChange(field: PoDynamicFormField, value: any) {
-    return this.sendChanges(field.validate, field, value).pipe(map(this.removeInvalidProperties()));
+    return this.sendChanges(field.validate, field, value);
   }
 
   sendFormChange(validate: Function | string, field: PoDynamicFormField, value: Array<any>) {
-    return this.sendChanges(validate, field, value).pipe(
-      map(validatedFields => {
-        const newFields = validatedFields.fields
-           .map(validatedField => mapObjectByProperties(validatedField, fieldProperties, true));
-        validatedFields.fields = newFields;
-
-        return validatedFields;
-      })
-    );
+    return this.sendChanges(validate, field, value);
   }
 
   updateFieldsForm(validatedFields: PoDynamicFormValidation, fields: Array<PoDynamicFormField>) {
-    const fieldsCopy = [ ...fields ];
-    const validatedFieldsCopy = [ ...validatedFields.fields ];
+    return [ ...validatedFields.fields ].reduce((updatedFields, validatedField) => {
+      const index = updatedFields.findIndex(field => field.property === validatedField.property);
+      const hasProperty = index >= 0;
 
-    validatedFieldsCopy.forEach((validatedField: PoDynamicFormField) => {
-      const index = fieldsCopy.findIndex(field => field.property === validatedField.property);
-      const hasThisProperty = index >= 0;
-
-      if (hasThisProperty) {
-        fieldsCopy[index] = { ...fields[index], ...validatedField };
+      if (hasProperty) {
+        updatedFields[index] = { ...fields[index], ...validatedField };
       } else {
-        fieldsCopy.push(validatedField);
+        updatedFields.push(validatedField);
       }
-    });
 
-    return fieldsCopy;
-  }
-
-  private removeInvalidProperties(): (value: any, index: number) => any {
-    return validatedField => {
-      const newField = mapObjectByProperties(validatedField.field, fieldProperties, true);
-      return { ...validatedField, field: newField };
-    };
+      return updatedFields;
+    }, [ ...fields ]);
   }
 
   private validateFieldOnServer(url: string, changedValue: { value: any; property: string; }) {
