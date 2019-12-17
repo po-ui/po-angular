f3afdd5fd03aea7797739e169af6487e24dbead0
jhonyeduardo <jhonyeduardo96@gmail.com>
2019-12-17 15:01:32 -0300

    feat(dynamic-form): adiciona metodos para inicialização
    Adiciona a propriedade: p-load, que possibilita
    executar uma função ou serviço na inicialização do componente, que pode retornar novos dados
    para atualizar o mesmo.
    
    Fixes DTHFUI-2416

---
+++ projects/ui/src/lib/components/po-dynamic/po-dynamic-form/po-dynamic-form-operation/po-dynamic-form-operation.spec.ts
@@ -0,0 +1,79 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+
+import { PoDynamicFormOperation } from './po-dynamic-form-operation';
+
+describe('PoDynamicFormOperation:', () => {
+  let dynamicFormOperation;
+
+  let httpMock: HttpTestingController;
+
+  const mockURL: string = 'http://po.portinari.com.br/api';
+  const value = { name: 'username' };
+
+  const spyLoadFunction = jasmine.createSpy('loadFunction');
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
+
+    httpMock = TestBed.get(HttpTestingController);
+
+    const http = TestBed.get(HttpClient);
+
+    dynamicFormOperation = new PoDynamicFormOperation(http);
+  });
+
+  describe('Methods:', () => {
+
+    it('execute: should call `post` if `load` param is a string', () => {
+      const spyPost = spyOn(dynamicFormOperation, 'post');
+
+      dynamicFormOperation['execute'](mockURL, value);
+
+      expect(spyPost).toHaveBeenCalledWith(mockURL, value);
+    });
+
+    it('execute: should call the function passed as param if `load` isn`t a string', () => {
+      const spyPost = spyOn(dynamicFormOperation, <any>'post');
+
+      dynamicFormOperation['execute'](spyLoadFunction, value);
+
+      expect(spyLoadFunction).toHaveBeenCalledWith(value);
+      expect(spyPost).not.toHaveBeenCalledWith();
+    });
+
+    it('post: should call `POST` method', () => {
+      const param = { property: 'name', value };
+
+      dynamicFormOperation['post'](mockURL, param).subscribe(response => {
+        expect(response).toBeDefined();
+      });
+
+      const req = httpMock.expectOne(`${mockURL}`);
+      expect(req.request.method).toBe('POST');
+    });
+
+    it('setFormDefaultIfEmpty: should return value if its is defined', () => {
+      const validatedField = { value: 'new value' };
+
+      expect(dynamicFormOperation['setFormDefaultIfEmpty'](validatedField)).toEqual(validatedField);
+    });
+
+    it('setFormDefaultIfEmpty: should return default form if its is undefined', () => {
+      const defaultForm = {
+        value: {},
+        fields: [],
+        focus: undefined
+      };
+
+      expect(dynamicFormOperation['setFormDefaultIfEmpty'](undefined)).toEqual(defaultForm);
+    });
+
+  });
+
+});
---
+++ projects/ui/src/lib/components/po-dynamic/po-dynamic-form/samples/sample-po-dynamic-form-register/sample-po-dynamic-form-register.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class PoDynamicFormRegisterService {
+
+  getCity(state: number) {
+    switch (state) {
+      case 1: {
+        return [
+          { label: 'Palhoça', value: 5 },
+          { label: 'Lages', value: 6 },
+          { label: 'Balneário Camboriú', value: 7 },
+          { label: 'Brusque', value: 8 },
+        ];
+      }
+      case 2: {
+        return [
+          { label: 'São Paulo', value: 9 },
+          { label: 'Guarulhos', value: 10 },
+          { label: 'Campinas', value: 11 },
+          { label: 'São Bernardo do Campo', value: 12 }
+        ];
+      }
+      case 3: {
+        return [
+          { label: 'Rio de Janeiro', value: 13 },
+          { label: 'São Gonçalo', value: 14 },
+          { label: 'Duque de Caxias', value: 15 },
+          { label: 'Nova Iguaçu', value: 16 }
+        ];
+      }
+      case 4: {
+        return [
+          { label: 'Belo Horizonte', value: 17 },
+          { label: 'Uberlândia', value: 18 },
+          { label: 'Contagem', value: 19 },
+          { label: 'Juiz de Fora', value: 20 }
+        ];
+      }
+    }
+    return [];
+  }
+
+  getUserDocument(value) {
+    const cpfField =  { property: 'cpf', visible: true };
+    const cnpjField =  { property: 'cnpj', visible: true };
+    const document = value.isJuridicPerson ? cnpjField : cpfField;
+
+    return {
+      fields: [ document ]
+    };
+  }
+
+}
