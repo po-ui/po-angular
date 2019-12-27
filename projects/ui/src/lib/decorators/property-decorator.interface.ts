7882f072b437b9cc20112eb0e780c19e83a581b9
Nicole Oliveira <nicoleoliveira1995@gmail.com>
2019-12-27 16:23:59 -0300

    feat(decorator): novo InputBoolean e InputRequired
    Implementa os novos decorators InputBoolean e InputRequired
    para validação interna dos componentes.
    
    Fixes DTHFUI-2578

---
+++ projects/ui/src/lib/decorators/property-decorator.interface.ts
@@ -0,0 +1,17 @@
+/**
+ * @description
+ *
+ * Estrutura dos parâmetros de um decorator de propriedade.
+ */
+export interface PropertyDecoratorInterface {
+
+  /** Métodos de acesso a propriedade, por exemplo: `get` e `set`. */
+  originalDescriptor?: Object;
+
+  /** Nome da propriedade. */
+  property?: string;
+
+  /** Instância da classe da propriedade. */
+  target?: any;
+
+}
