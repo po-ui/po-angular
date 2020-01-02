30ac592aeb075e3cd0bdc5305951f45060d33bdf
jhonyeduardo <jhonyeduardo96@gmail.com>
2020-01-02 11:27:02 -0300

    perf(table): implementa alguns itens de performance
    Foram revistos alguns itens, tentando não
    dificultar muito a revisão/funcionamento, são eles:
    
    - OnPush em alguns componentes (de exibição) internos.
    - trackBy;
    - Chamadas de funções desnecessarias no template;
    - Removido os ng-templates de dentro dos ngFor;
    
    Também foi reordenado as funções;
    
    Proxímos passos:
    - Componentizar mais o componente internamente; ex: po-table-header
    - Avaliar utilizar apenas dados imutaveis;
    
    Fixes DTHFUI-2571

--- projects/ui/src/lib/components/po-table/po-table-icon/po-table-icon.component.spec.ts
+++ projects/ui/src/lib/components/po-table/po-table-icon/po-table-icon.component.spec.ts
@@ -25,7 +25,6 @@ describe('PoTableIconComponent:', () => {
     fixture = TestBed.createComponent(PoTableIconComponent);
     component = fixture.componentInstance;
 
-    fixture.detectChanges();
     nativeElement = fixture.debugElement.nativeElement;
   });
 
