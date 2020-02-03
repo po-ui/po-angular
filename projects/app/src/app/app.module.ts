6cdcf95208eadbd0ca3859414694f45197f575d1
Nicole Oliveira <nicoleoliveira1995@gmail.com>
2020-02-03 16:01:20 -0300

    feat(spike-menu): semantica correção

--- projects/app/src/app/app.module.ts
+++ projects/app/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { PoModule } from '@portinari/portinari-ui';
 import { AppComponent } from './app.component';
 // tslint:disable-next-line: max-line-length
 import { SamplePoMenuHumanResourcesComponent } from 'projects/ui/src/lib/components/po-menu/samples/sample-po-menu-human-resources/sample-po-menu-human-resources.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { SamplePoMenuHumanResourcesComponent } from 'projects/ui/src/lib/compone
   imports: [
     BrowserModule,
     FormsModule,
+    BrowserAnimationsModule,
     RouterModule.forRoot([]),
     PoModule
   ],
