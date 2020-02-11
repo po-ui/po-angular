jhonyeduardo/portinari-angular

feat(progress): cria o componente po-progress
Implementa o novo componente po-progress contendo
o po-progress-bar.

Fixes DTHFUI-1435

projects/ui/src/lib/components/po-progress/samples/sample-po-progress-labs/sample-po-progress-labs.component.ts
@@ -1,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+
+import { PoCheckboxGroupOption, PoProgressStatus, PoRadioGroupOption } from '@portinari/portinari-ui';
+
+@Component({
+  selector: 'sample-po-progress-labs',
+  templateUrl: './sample-po-progress-labs.component.html',
+})
+export class SamplePoProgressLabsComponent implements OnInit {
+
+  event: any;
+  info: string;
+  infoIcon: string;
+  properties: Array<string>;
+  status: PoProgressStatus;
+  text: string;
+  value: number;
+
+  infoIconsOptions: Array<PoRadioGroupOption> = [
+    { label: 'po-icon-portinari', value: 'po-icon-portinari' },
+    { label: 'po-icon-calendar', value: 'po-icon-calendar' },
+    { label: 'po-icon-user', value: 'po-icon-user' },
+    { label: 'po-icon-telephone', value: 'po-icon-telephone' }
+  ];
+
+  propertiesOptions: Array<PoCheckboxGroupOption> = [
+    { label: 'Indeterminate', value: 'indeterminate' }
+  ];
+
+  statusOptions: Array<PoRadioGroupOption> = [
+    { label: 'Default', value: PoProgressStatus.Default },
+    { label: 'Success', value: PoProgressStatus.Success },
+    { label: 'Error', value: PoProgressStatus.Error }
+  ];
+
+  ngOnInit() {
+    this.restore();
+  }
+
+  cancel() {
+    this.event = 'cancel';
+  }
+
+  restore() {
+    this.event = undefined;
+    this.info = undefined;
+    this.infoIcon = undefined;
+    this.properties = [];
+    this.status = undefined;
+    this.text = undefined;
+    this.value = undefined;
+  }
+
+}

po-ui/po-angular

projects/ui/src/lib/components/po-progress/samples/sample-po-progress-labs/sample-po-progress-labs.component.ts
@@ -1,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+
+import { PoCheckboxGroupOption, PoProgressStatus, PoRadioGroupOption } from '@portinari/portinari-ui';
+
+@Component({
+  selector: 'sample-po-progress-labs',
+  templateUrl: './sample-po-progress-labs.component.html',
+})
+export class SamplePoProgressLabsComponent implements OnInit {
+
+  event: any;
+  info: string;
+  infoIcon: string;
+  properties: Array<string>;
+  status: PoProgressStatus;
+  text: string;
+  value: number;
+
+  infoIconsOptions: Array<PoRadioGroupOption> = [
+    { label: 'po-icon-exclamation', value: 'po-icon-exclamation' },
+    { label: 'po-icon-ok', value: 'po-icon-ok' },
+    { label: 'po-icon-user', value: 'po-icon-user' },
+    { label: 'po-icon-no-signal', value: 'po-icon-no-signal' }
+  ];
+
+  propertiesOptions: Array<PoCheckboxGroupOption> = [
+    { label: 'Indeterminate', value: 'indeterminate' }
+  ];
+
+  statusOptions: Array<PoRadioGroupOption> = [
+    { label: 'Default', value: PoProgressStatus.Default },
+    { label: 'Success', value: PoProgressStatus.Success },
+    { label: 'Error', value: PoProgressStatus.Error }
+  ];
+
+  ngOnInit() {
+    this.restore();
+  }
+
+  cancel() {
+    this.event = 'cancel';
+  }
+
+  restore() {
+    this.event = undefined;
+    this.info = undefined;
+    this.infoIcon = undefined;
+    this.properties = [];
+    this.status = undefined;
+    this.text = undefined;
+    this.value = undefined;
+  }
+
+}