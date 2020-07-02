89a58f96b3985433529b22dbbe93599657bd336a
Maicon Funke <maicon.funke@gmail.com>
2020-07-02 11:35:25 -0300

    feat(table): implementa opção para mudar a posição do detail
    Esta implementação permite que o usuário escolha a posição da arrow
    do detail quando se esta utilizado a diretiva `p-table-row-template`.
    Possibilitando o mesmo deixar como `LEFT` (padrão) ou `RIGHT`.
    
    FIX DTHFUI-3718

---
+++ projects/ui/src/lib/components/po-table/enums/po-table-row-template-arrow-direction.enum.ts
@@ -0,0 +1,13 @@
+/**
+ * @usedBy PoTableRowTemplateDirective
+ *
+ * @description
+ * Define a posição da arrow que expande o * row template* na tabela, será exibida na esquerda ou direita.
+ */
+export enum PoTableRowTemplateArrowDirection {
+  /** Posiciona a *arrow* na esquerda. (Padrão) */
+  Left = 'LEFT',
+
+  /** Posiciona a *arrow* na direita */
+  Right = 'RIGHT'
+}
--- projects/ui/src/lib/components/po-table/po-table-row-template/po-table-row-template.directive.spec.ts
+++ projects/ui/src/lib/components/po-table/po-table-row-template/po-table-row-template.directive.spec.ts
@@ -1,10 +1,26 @@
 import { PoTableRowTemplateDirective } from './po-table-row-template.directive';
+import { PoTableRowTemplateArrowDirection } from '../enums/po-table-row-template-arrow-direction.enum';
 
 describe('PoTableRowTemplateDirective', () => {
   const component = new PoTableRowTemplateDirective(null);
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('tableRowTemplateArrowDirection: should be right', () => {
+    component.tableRowTemplateArrowDirection = PoTableRowTemplateArrowDirection.Right;
+    expect(component.tableRowTemplateArrowDirection).toEqual('RIGHT');
+  });
+
+  it('tableRowTemplateArrowDirection: should be left', () => {
+    component.tableRowTemplateArrowDirection = PoTableRowTemplateArrowDirection.Left;
+    expect(component.tableRowTemplateArrowDirection).toEqual('LEFT');
+  });
+
+  it('tableRowTemplateArrowDirection: should be left when value is null', () => {
+    component.tableRowTemplateArrowDirection = null;
+    expect(component.tableRowTemplateArrowDirection).toEqual('LEFT');
   });
 
   describe('Methods: ', () => {
