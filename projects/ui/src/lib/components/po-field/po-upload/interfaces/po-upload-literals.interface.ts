3f5bf3ff08e5ef9028424385b610ed565d8cda8a
Samir Hassan Ayoub <samir.ayoub@SPOMM010114176.local>
2020-07-09 14:30:18 -0300

    refactor(upload): remove literais
    Foram removidas literais do componente.
    
    Fixes DTHFUI-3692
    
    BREAKING CHANGES: removidas literais
    
    Foram removidas as literais ‘cancel’, ‘deleteFile’ e ‘tryAgain’ do componente.

--- projects/ui/src/lib/components/po-field/po-upload/interfaces/po-upload-literals.interface.ts
+++ projects/ui/src/lib/components/po-field/po-upload/interfaces/po-upload-literals.interface.ts
@@ -6,28 +6,6 @@
  * Interface para definição das literais usadas no `po-upload`.
  */
 export interface PoUploadLiterals {
-  /**
-   * @deprecated 3.x.x
-   *
-   * @description
-   *
-   * ***Deprecated 3.x.x***
-   *
-   * Texto exibido no label para cancelar o envio.
-   */
-  cancel?: string;
-
-  /**
-   * @deprecated 3.x.x
-   *
-   * @description
-   *
-   * ***Deprecated 3.x.x***
-   *
-   * Texto exibido no label para excluir o arquivo.
-   */
-  deleteFile?: string;
-
   /** Texto indicativo para a área onde os arquivos devem ser arrastados quando utilizada a propriedade `p-drag-drop`. */
   dragFilesHere?: string;
 
@@ -72,17 +50,6 @@ export interface PoUploadLiterals {
 
   /** Texto exibido no label do botão para iniciar o envio dos arquivos. */
   startSending?: string;
-
-  /**
-   * @deprecated 3.x.x
-   *
-   * @description
-   *
-   * ***Deprecated 3.x.x***
-   *
-   * Texto exibido no label para tentar novamente.
-   */
-  tryAgain?: string;
 
   /** Texto a ser exibido quando ocorrer erro no envio do arquivo. */
   errorOccurred?: string;
