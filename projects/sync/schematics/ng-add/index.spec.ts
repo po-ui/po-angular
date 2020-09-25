a26d5cff742b9e68e5c116b256e6e63da8af0c6f
alinelariguet <alinecbgmoreira@gmail.com>
2020-09-25 11:42:10 -0300

    feat(schematics): adiciona testes unitários para o @po-ui/ng-sync
    Não era possível executar os testes unitários dos schematics ng add e ng generate do pacote ng-sync.
    Também adiciona no CI a execução destes testes.
    
    Fixes DTHFUI-3459

---
+++ projects/sync/schematics/ng-add/index.spec.ts
@@ -0,0 +1,79 @@
+import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
+import { Schema as WorkspaceOptions } from '@schematics/angular/workspace/schema';
+import { Tree } from '@angular-devkit/schematics';
+
+import * as path from 'path';
+
+const collectionPath = path.join(__dirname, '../collection.json');
+
+describe('Schematic: ng-add', () => {
+  const runner = new SchematicTestRunner('schematics', collectionPath);
+
+  const workspaceOptions: WorkspaceOptions = {
+    name: 'workspace',
+    newProjectRoot: 'projects',
+    version: '6.0.0'
+  };
+
+  const componentOptions: any = {
+    name: 'po',
+    appName: 'po'
+  };
+
+  let appTree: UnitTestTree;
+  let infoOutput: Array<string>;
+
+  beforeEach(async () => {
+    appTree = await runner.runExternalSchematicAsync('@schematics/angular', 'workspace', workspaceOptions).toPromise();
+    appTree = await runner
+      .runExternalSchematicAsync('@schematics/angular', 'application', componentOptions, appTree)
+      .toPromise();
+
+    infoOutput = [];
+    runner.logger.subscribe(e => {
+      if (e.level === 'info') {
+        infoOutput.push(e.message);
+      }
+    });
+  });
+
+  describe('Dependencies:', () => {
+    it('should update package.json with @po-ui/ng-sync dependencies and run nodePackageInstall', async () => {
+      const tree = await runner.runSchematicAsync('ng-add', componentOptions, appTree).toPromise();
+      const packageJson = JSON.parse(getFileContent(tree, '/package.json'));
+      const dependencies = packageJson.dependencies;
+      const info =
+        'Sync added successfully, please execute the command `ionic cordova plugin add cordova-plugin-network-information`';
+
+      expect(dependencies['@po-ui/ng-sync']).toBeDefined();
+      expect(Object.keys(dependencies)).toEqual(Object.keys(dependencies).sort());
+      expect(runner.tasks.some(task => task.name === 'node-package')).toBe(true);
+      expect(infoOutput.length).toBe(1);
+      expect(infoOutput[0]).toMatch(new RegExp(`${info}`));
+    });
+  });
+
+  describe('Imports:', () => {
+    it('should add the PoSyncModule and PoStorageModule to the project module', async () => {
+      const poSyncModuleName = 'PoSyncModule';
+      const poStorageModuleName = 'PoStorageModule';
+
+      const tree = await runner.runSchematicAsync('ng-add', componentOptions, appTree).toPromise();
+      const fileContent = getFileContent(tree, `projects/${componentOptions.appName}/src/app/app.module.ts`);
+
+      expect(fileContent).toContain(poSyncModuleName);
+      expect(fileContent).toContain(poStorageModuleName);
+    });
+  });
+});
+
+/** Gets the content of a specified file from a schematic tree. */
+function getFileContent(tree: Tree, filePath: string): string {
+  const contentBuffer = tree.read(filePath);
+
+  if (!contentBuffer) {
+    throw new Error(`Cannot read "${filePath}" because it does not exist.`);
+  }
+
+  return contentBuffer.toString();
+}
---
+++ projects/sync/schematics/ng-generate/schema/index.spec.ts
@@ -0,0 +1,62 @@
+import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
+import { Schema as WorkspaceOptions } from '@schematics/angular/workspace/schema';
+import { Tree } from '@angular-devkit/schematics';
+
+import * as path from 'path';
+
+const collectionPath = path.join(__dirname, '../../collection.json');
+
+describe('schema:', () => {
+  const runner = new SchematicTestRunner('schematics', collectionPath);
+
+  const workspaceOptions: WorkspaceOptions = {
+    name: 'workspace',
+    newProjectRoot: 'projects',
+    version: '6.0.0'
+  };
+
+  const componentOptions: any = {
+    name: 'po',
+    project: 'po'
+  };
+
+  let appTree: UnitTestTree;
+
+  beforeEach(async () => {
+    appTree = await runner.runExternalSchematicAsync('@schematics/angular', 'workspace', workspaceOptions).toPromise();
+    appTree = await runner
+      .runExternalSchematicAsync('@schematics/angular', 'application', componentOptions, appTree)
+      .toPromise();
+  });
+
+  it('should create sync schema', async () => {
+    const schemaName = 'supply';
+    const tree = await runner
+      .runSchematicAsync('schema', { ...componentOptions, name: schemaName }, appTree)
+      .toPromise();
+
+    const files: Array<string> = tree.files;
+
+    const fullFilePath = `/projects/${componentOptions.project}/src/app/${schemaName}/${schemaName}.constants.ts`;
+
+    const schemaContent = getFileContent(
+      tree,
+      `/projects/${componentOptions.project}/src/app/${schemaName}/${schemaName}.constants.ts`
+    );
+
+    expect(files).toContain(fullFilePath);
+    expect(schemaContent).toMatch(new RegExp(`name: '${schemaName}'`));
+    expect(schemaContent).toMatch(new RegExp(`export const ${schemaName}Schema: PoSyncSchema`));
+  });
+
+  /** Gets the content of a specified file from a schematic tree. */
+  function getFileContent(tree: Tree, filePath: string): string {
+    const contentBuffer = tree.read(filePath);
+
+    if (!contentBuffer) {
+      throw new Error(`Cannot read "${filePath}" because it does not exist.`);
+    }
+
+    return contentBuffer.toString();
+  }
+});
