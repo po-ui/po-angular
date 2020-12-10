95540634890ab44fd3141a9e73c4ebda98dd3a0f
Danilo Sérvulo Bispo <48370247+DaniloServulo@users.noreply.github.com>
2020-12-09 23:03:51 -0300

    feat(page-default|list): permite utilizar a propriedade visible
    Permite realizar a exibição do campos caso o visible esteja como true lembrando que este campo pode ser utilizado como opcional. Caso não utilize este campo visible para determinar se ele será exibido ou não, ele será exibido.
    
    Fixes DTHFUI-2145, #117

--- projects/ui/src/lib/components/po-page/po-page-action.interface.ts
+++ projects/ui/src/lib/components/po-page/po-page-action.interface.ts
@@ -4,7 +4,7 @@ import { PoPopupAction } from '../po-popup/po-popup-action.interface';
  * @description
  * Interface para as ações dos componentes po-page-default e po-page-list.
  *
- * > As propriedades `selected`, `separator`, `type` e `visible` serão vistas a partir da terceira ação e somente quando
+ * > As propriedades `selected`, `separator` e `type` serão vistas a partir da terceira ação e somente quando
  * definir quatro ações ou mais.
  *
  * @docsExtends PoPopupAction
