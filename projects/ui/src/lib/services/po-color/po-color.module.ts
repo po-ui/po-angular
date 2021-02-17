89112cb25a25b5226d6cb0c83e5a369a7aa61e5c
Samir Ayoub <123samir@gmail.com>
2021-02-17 17:12:55 -0300

    feat(color): criado PoColorService para tratamento de cores
    O `PoColorService` verifica se na lista passada há uma definição para color. Se não houver, adiciona propriedade color e aplica a cor default.
    
    Fixes DTHFUI-4722

---
+++ projects/ui/src/lib/services/po-color/po-color.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { PoColorService } from './po-color.service';
+
+/**
+ * @description
+ *
+ * Módulo do serviço `po-color`.
+ */
+@NgModule({
+  providers: [PoColorService],
+  bootstrap: []
+})
+export class PoColorModule {}
---
+++ projects/ui/src/lib/services/po-color/po-color.service.spec.ts
@@ -0,0 +1,102 @@
+import { TestBed } from '@angular/core/testing';
+
+import { PoColorService } from './po-color.service';
+import { PoDefaultColors } from './po-colors.constant';
+
+describe('PoColorService', () => {
+  let service: PoColorService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(PoColorService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe('Methods:', () => {
+    describe('getColors:', () => {
+      it('should apply value to `defaultColors` if data doesn`t contain property `color`', () => {
+        const data = [{ from: 0, to: 50 }];
+
+        service.getColors<any>(data);
+
+        expect(service.defaultColors).toEqual(PoDefaultColors[0]);
+      });
+
+      it('should apply value to `defaultColors` if one of data list doesn`t contain property `color`', () => {
+        const data = [
+          { from: 0, to: 50, color: 'red' },
+          { from: 50, to: 100 }
+        ];
+
+        service.getColors(data);
+
+        expect(service.defaultColors).toEqual(PoDefaultColors[1]);
+      });
+
+      it('should apply value to `defaultColors` if data has a lengh greater than 12 and doesn`t have property color', () => {
+        const data = [
+          { from: 0, to: 10 },
+          { from: 20, to: 30 },
+          { from: 30, to: 40 },
+          { from: 40, to: 50 },
+          { from: 50, to: 60 },
+          { from: 60, to: 70 },
+          { from: 70, to: 80 },
+          { from: 80, to: 90 },
+          { from: 90, to: 100 },
+          { from: 100, to: 110 },
+          { from: 110, to: 120 },
+          { from: 120, to: 130 },
+          { from: 130, to: 140 },
+          { from: 140, to: 150 }
+        ];
+
+        service.getColors<any>(data);
+
+        expect(service.defaultColors.length).toEqual(14);
+      });
+
+      it('shouldn`t apply value to `defaultColors` if data list have property `color`', () => {
+        const data = [
+          { from: 0, to: 50, color: 'red' },
+          { from: 50, to: 100, color: 'blue' }
+        ];
+
+        service.getColors(data);
+
+        expect(service.defaultColors).toEqual([]);
+      });
+
+      it('should return a list of data containing default colors', () => {
+        const data = [
+          { from: 0, to: 50 },
+          { from: 50, to: 100 }
+        ];
+        const expectedResult = [
+          { from: 0, to: 50, color: PoDefaultColors[1][0] },
+          { from: 50, to: 100, color: PoDefaultColors[1][1] }
+        ];
+
+        expect(service.getColors<any>(data)).toEqual(expectedResult);
+      });
+
+      it('should return a list of data with defaultColors, po-color and the passed color', () => {
+        const data = [
+          { from: 0, to: 30 },
+          { from: 30, to: 60, color: 'red' },
+          { from: 60, to: 100, color: 'color-01' }
+        ];
+        const expectedResult = [
+          { from: 0, to: 30, color: PoDefaultColors[2][0] },
+          { from: 30, to: 60, color: 'red' },
+          { from: 60, to: 100, color: 'po-color-01' }
+        ];
+
+        expect(service.getColors(data)).toEqual(expectedResult);
+      });
+    });
+  });
+});
