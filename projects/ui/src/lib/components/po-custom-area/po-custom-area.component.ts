4dd6e27ddc58fce86ac67962d9d70b4ccb28fb37
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2021-03-17 15:32:45 -0300

    fix: criação do spike

---
+++ projects/ui/src/lib/components/po-custom-area/po-custom-area.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'po-po-custom-area',
+  templateUrl: './po-custom-area.component.html',
+  styleUrls: ['./po-custom-area.component.css']
+})
+export class PoCustomAreaComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
