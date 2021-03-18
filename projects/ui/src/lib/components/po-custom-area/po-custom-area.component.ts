8194aec54ea042686cdf835165bc5fc2ceadc6bd
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2021-03-18 15:23:33 -0300

    fix: inclusão do componente

--- projects/ui/src/lib/components/po-custom-area/po-custom-area.component.ts
+++ projects/ui/src/lib/components/po-custom-area/po-custom-area.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { PoCustomAreaService } from './services/po-custom-area.service';
 
 @Component({
   selector: 'po-po-custom-area',
@@ -6,7 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./po-custom-area.component.css']
 })
 export class PoCustomAreaComponent implements OnInit {
-  constructor() {}
+  @Input('p-api-custom') apiCustom: string;
+  @Input('p-component-name') componentName: string;
+  @Input('p-props') props: { [key: string]: string };
+  @Input('p-class-style') classStyle: string;
+  @Input('p-events') events: { [key: string]: (event: any) => void };
+  @Input('p-slot-text') slotText: string;
 
-  ngOnInit(): void {}
+  constructor(private poCustomAreaService: PoCustomAreaService) {}
+
+  ngOnInit(): void {
+    this.poCustomAreaService.api = this.apiCustom;
+    this.poCustomAreaService.add(
+      this.componentName,
+      'content',
+      this.props,
+      this.classStyle,
+      this.events,
+      this.slotText
+    );
+  }
 }
--- projects/ui/src/lib/components/po-custom-area/services/po-custom-area.service.ts
+++ projects/ui/src/lib/components/po-custom-area/services/po-custom-area.service.ts
@@ -1,9 +1,104 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
+import { CustomComponent, CustomComponents } from '../po-custom-area.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PoCustomAreaService {
   constructor(private http: HttpClient) {}
+
+  //:TODO: Implementar o método de set da API do I18N
+  private _api: string;
+
+  loaded: CustomComponents = [];
+  componentsAdded: Array<any> = [];
+
+  get api() {
+    return this._api;
+  }
+
+  set api(api: string) {
+    this._api = api;
+  }
+
+  private load(component: string): Observable<boolean> {
+    if (this.loaded.find(custom => custom.component === component)) {
+      return of(true);
+    }
+
+    return this.http.get<CustomComponent>(`${this.api}/${component}`).pipe(
+      map(custom => ({ ...custom, src: atob(custom.src) })),
+      map(custom => {
+        const { src, ...customNoSrc } = custom;
+        this.loaded.push(customNoSrc);
+        const script = document.createElement('script');
+        script.text = src;
+        document.body.appendChild(script);
+        return true;
+      })
+    );
+  }
+
+  private createComponent(
+    tileKind: string,
+    element: string,
+    props?: { [key: string]: string },
+    classStyle?: string,
+    events?: { [key: string]: (event: any) => void },
+    slotText?: string
+  ) {
+    const tile = document.createElement(tileKind);
+
+    if (props) {
+      Object.keys(props).forEach(prop => {
+        tile.setAttribute(prop, props[prop]);
+      });
+    }
+
+    if (events) {
+      Object.keys(events).forEach(event => {
+        tile.addEventListener(event, events[event]);
+      });
+    }
+
+    if (classStyle) {
+      tile.setAttribute('class', classStyle);
+    }
+
+    if (slotText) {
+      tile.innerHTML = slotText;
+    }
+
+    const content = document.getElementById(element);
+    content.appendChild(tile);
+
+    return tile;
+  }
+
+  add(
+    tileKind: string,
+    element: string,
+    props?: { [key: string]: string },
+    classStyle?: string,
+    events?: { [key: string]: (event: any) => void },
+    slotText?: string
+  ): Observable<boolean> {
+    return this.load(tileKind).pipe(
+      tap(loaded => {
+        if (loaded) {
+          const tile = this.createComponent(tileKind, element, props, classStyle, events, slotText);
+          this.componentsAdded.push(tile);
+        }
+      })
+    );
+  }
+
+  notifyAll(props) {
+    this.componentsAdded.forEach(component => {
+      component.notify(props);
+    });
+  }
 }
