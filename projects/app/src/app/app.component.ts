4253981593de5d8f09918db40c6d33d350aa068e
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2021-03-19 17:43:02 -0300

    fix: correcao da poc

--- projects/app/src/app/app.component.ts
+++ projects/app/src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { PoPageDynamicTableActions } from 'projects/templates/src/lib';
 import { PoBreadcrumb } from '@po-ui/ng-components/lib';
+import { PoMenuItem } from '../../../ui/src/lib';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+  readonly menus: Array<PoMenuItem> = [
+    { label: 'Cliente', link: '/cliente' },
+    { label: 'Teste', link: '/teste' }
+  ];
+}
