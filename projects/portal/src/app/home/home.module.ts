d9b7ae2b46d1fbc22a69e733165e134dc944bae7
Bruno Romeiro <bruno.romeiro@gmail.com>
2021-03-25 19:00:47 -0300

    feat(portal): automatiza a atualização de estrelas do projeto
    Automatiza a atualização de stars do projeto no Github

--- projects/portal/src/app/home/home.module.ts
+++ projects/portal/src/app/home/home.module.ts
@@ -5,11 +5,12 @@ import { SharedModule } from './../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { HomeColorfulComponent } from './home-colorful/home-colorful.component';
+import { HomeColorfulService } from './home-colorful/home-colorful.service';
 
 @NgModule({
   imports: [SharedModule, HomeRoutingModule],
   declarations: [HomeComponent, HomeColorfulComponent],
-  providers: [],
+  providers: [HomeColorfulService],
   bootstrap: []
 })
 export class HomeModule {}
