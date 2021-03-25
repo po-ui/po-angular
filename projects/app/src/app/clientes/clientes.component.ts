fcc4c47cb7a546bd8a18e830b28b130b4d5983e9
Alvaro Camillo Neto <alvaro.camilloneto@gmail.com>
2021-03-25 09:33:44 -0300

    fix: correção

--- projects/app/src/app/clientes/clientes.component.ts
+++ projects/app/src/app/clientes/clientes.component.ts
@@ -1,57 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 
-import { PoBreadcrumb } from 'projects/ui/src/lib';
-import { PoPageDynamicTableActions, PoPageDynamicTableBeforeRemoveAll } from 'projects/templates/src/lib';
-
+import { NgForm } from '@angular/forms';
+import {
+  PoNotificationService,
+  PoDialogService,
+  PoBreadcrumb,
+  PoCustomAreaService,
+  PoRadioGroupOption
+} from '../../../../ui/src/lib';
+import { Router } from '@angular/router';
 @Component({
   templateUrl: './clientes.component.html'
 })
 export class ClientesComponent {
-  public readonly serviceApi = 'http://localhost:3000/v1/people';
+  birthDate: Date;
+  email: string;
+  fathersName: string;
+  genre: string;
+  graduation: string;
+  mothersName: string;
+  name: string;
+  nationality: string;
+  nickname: string;
+  placeOfBirth: string;
+  userId: number;
+  personType: string = 'CNPJ';
+  cnpj: string;
 
-  public readonly actions: PoPageDynamicTableActions = {
-    detail: 'dynamic-detail/:id',
-    duplicate: 'dynamic-new',
-    edit: 'dynamic-edit/:id',
-    new: 'dynamic-new', //this.newResource.bind(this),
-    remove: true,
-    removeAll: this.deleteAll.bind(this),
-    beforeRemoveAll: this.onBeforeDeleteAll.bind(this)
-  };
+  readonly personOptions: Array<PoRadioGroupOption> = [
+    { label: 'Jurídica', value: 'CNPJ' },
+    { label: 'Física', value: 'CPF' }
+  ];
+  eventInput = { emitValue: this.setCNPJ.bind(this) };
 
   public readonly breadcrumb: PoBreadcrumb = {
-    items: [{ label: 'Home', link: '/' }, { label: 'People' }]
+    items: [{ label: 'Home', action: this.beforeRedirect.bind(this) }, { label: 'User Edit' }]
   };
 
-  deleteAll(resources) {
-    console.log('Remove All');
-    console.log(resources);
-    return resources;
+  @ViewChild('formEditUser', { static: true }) formEditUser: NgForm;
+
+  constructor(
+    private route: Router,
+    private poDialog: PoDialogService,
+    private poNotification: PoNotificationService,
+    private poCustomAreaService: PoCustomAreaService
+  ) {}
+
+  ngOnInit() {
+    this.initialize();
   }
 
-  onBeforeDeleteAll(resources): PoPageDynamicTableBeforeRemoveAll {
-    console.log('Before');
-    console.log(resources);
-    return {
-      resources: [],
-      allowAction: true
-    };
+  cancel() {
+    this.initialize();
   }
 
-  public readonly cityOptions: Array<object> = [
-    { value: 'São Paulo', label: 'São Paulo' },
-    { value: 'Joinville', label: 'Joinville' },
-    { value: 'São Bento', label: 'São Bento' },
-    { value: 'Araquari', label: 'Araquari' },
-    { value: 'Campinas', label: 'Campinas' },
-    { value: 'Osasco', label: 'Osasco' }
-  ];
+  setCNPJ(event) {
+    const { value } = event.detail;
+    this.cnpj = value;
+  }
 
-  public readonly fields: Array<any> = [
-    { property: 'id', key: true },
-    { property: 'name', label: 'Name', filter: true, gridColumns: 6 },
-    { property: 'genre', label: 'Genre', filter: true, gridColumns: 6, duplicate: true },
-    { property: 'birthdate', label: 'Birthdate', type: 'date', gridColumns: 6 },
-    { property: 'city', label: 'City', filter: true, duplicate: true, options: this.cityOptions, gridColumns: 12 }
-  ];
+  initialize() {
+    this.birthDate = new Date(1978, 11, 26);
+    this.email = 'john.doe@po-ui.com.br';
+    this.fathersName = 'Mike Doe';
+    this.genre = 'male';
+    this.graduation = 'College Degree';
+    this.mothersName = 'Jane Doe';
+    this.name = 'John Doe';
+    this.nationality = 'USA';
+    this.nickname = 'John';
+    this.placeOfBirth = 'Colorado';
+    this.userId = 122635;
+  }
+
+  save() {
+    this.poNotification.success(`Save successfully`);
+  }
+
+  private beforeRedirect(itemBreadcrumbLabel) {
+    if (this.formEditUser.valid) {
+      this.route.navigate(['/']);
+    } else {
+      this.poDialog.confirm({
+        title: `Confirm redirect to ${itemBreadcrumbLabel}`,
+        message: `There is data that has not been saved yet. Are you sure you want to quit?`,
+        confirm: () => this.route.navigate(['/'])
+      });
+    }
+  }
+
+  changePerson(personType) {
+    this.poCustomAreaService.notifyAll({ personType });
+  }
 }
