bfa0c4dd52bb0870287f00734efc8ad8931abc26
jhony.senem <jhonyeduardo96@gmail.com>
2021-05-28 02:14:12 -0300

    refactor(packages): atualiza configuração para utilizar karma-coverage
    Fixes DTHFUI-4978

--- projects/storage/karma.conf.js
+++ projects/storage/karma.conf.js
@@ -9,18 +9,23 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
+      require('karma-coverage'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
-    coverageIstanbulReporter: {
+    coverageReporter: {
       dir: require('path').join(__dirname, '../../coverage/storage'),
-      reports: ['html', 'lcovonly', 'text-summary', 'cobertura'],
-      fixWebpackSourcePaths: true,
-      thresholds: {
-        emitWarning: false, // set to 'true' to not fail the test command when thresholds are not met
+      subdir: '.',
+      watermarks: {
+        statements: [50, 75],
+        functions: [50, 75],
+        branches: [50, 75],
+        lines: [50, 75]
+      },
+      reporters: [{ type: 'html' }, { type: 'lcovonly' }, { type: 'text-summary' }, { type: 'cobertura' }],
+      check: {
         global: {
           statements: 99,
           branches: 99,
@@ -28,8 +33,8 @@ module.exports = function (config) {
           lines: 99
         },
         each: {
-          statements: 99,
           branches: 99,
+          statements: 99,
           lines: 99,
           functions: 99
         }
@@ -44,10 +49,11 @@ module.exports = function (config) {
     customLaunchers: {
       ChromeHeadless: {
         base: 'Chrome',
-        flags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222']
+        flags: ['--no-sandbox', '--headless', '--disable-gpu', '--disable-web-security', '--remote-debugging-port=9222']
       }
     },
     singleRun: false,
-    restartOnFileChange: true
+    restartOnFileChange: true,
+    browserNoActivityTimeout: 30000
   });
 };
