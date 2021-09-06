83fdee2d4ea69c798b3d47ba9372d69e455e7ca6
luizmariz <37978726+luizmariz@users.noreply.github.com>
2021-09-06 15:40:27 -0300

    feat(po-page-dynamic-table): adiciona icon a interface da custom action
    Define um ícone para as custom actions que são mapeadas para a interface PoPageAction do po-page-dynamic-search
    
    Fixes #987

--- projects/templates/src/lib/components/po-page-dynamic-table/interfaces/po-page-dynamic-table-custom-action.interface.ts
+++ projects/templates/src/lib/components/po-page-dynamic-table/interfaces/po-page-dynamic-table-custom-action.interface.ts
@@ -1,3 +1,5 @@
+import { TemplateRef } from '@angular/core';
+
 /**
  * @usedBy PoPageDynamicTableComponent
  *
@@ -40,4 +42,46 @@ export interface PoPageDynamicTableCustomAction {
    * o botão de ação caso nenhum recurso for selecionado.
    */
   selectable?: boolean;
+
+  /**
+   * @description
+   *
+   * Define um ícone que será exibido ao lado esquerdo do rótulo.
+   *
+   * É possível usar qualquer um dos ícones da [Biblioteca de ícones](/guides/icons). conforme exemplo abaixo:
+   * ```
+   * <po-component
+   *  [p-property]="[{ label: 'PO ICON', icon: 'po-icon-news' }]">
+   * </po-component>
+   * ```
+   *
+   * Também é possível utilizar outras fontes de ícones, por exemplo a biblioteca Font Awesome, da seguinte forma:
+   * ```
+   * <po-component
+   *  [p-property]="[{ label: 'FA ICON', icon: 'fa fa-icon-podcast' }]">
+   * </po-component>
+   * ```
+   *
+   * Outra opção seria a customização do ícone através do `TemplateRef`, conforme exemplo abaixo:
+   * component.html:
+   * ```
+   * <ng-template #iconTemplate>
+   *   <ion-icon name="heart"></ion-icon>
+   * </ng-template>
+   *
+   * <po-component [p-property]="myProperty"></po-component>
+   * ```
+   * component.ts:
+   * ```
+   * @ViewChild('iconTemplate', { static: true } ) iconTemplate : TemplateRef<void>;
+   *
+   * myProperty = [
+   *  {
+   *    label: 'FA ICON',
+   *    icon: this.iconTemplate
+   *  }
+   * ];
+   * ```
+   */
+  icon?: string | TemplateRef<void>;
 }
