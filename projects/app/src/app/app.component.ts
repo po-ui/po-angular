08211211bee2f9e1d5011a4677cbdf87d3b3e6cd
alinelariguet <alinecbgmoreira@gmail.com>
2021-09-22 16:48:03 -0300

    feat: inclui rich-text no app para teste

--- projects/app/src/app/app.component.ts
+++ projects/app/src/app/app.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
-import { PoButtonGroupItem } from '../../../ui/src/lib';
+import { PoButtonGroupItem, PoButtonGroupToggle } from '../../../ui/src/lib';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
 export class AppComponent {
+  toggle = PoButtonGroupToggle.Multiple;
   buttons: Array<PoButtonGroupItem> = [
-    { label: 'Button 1', action: this.action.bind(this) },
-    { label: 'Button 2', action: this.action.bind(this), tooltip: 'teste tooltip' }
+    { label: 'Button 1', action: this.action.bind(this), selected: true },
+    { label: 'Button 2', action: this.action.bind(this), tooltip: 'teste tooltip' },
+    { label: 'Button 3', action: this.action.bind(this), tooltip: 'teste tooltip 2', disabled: true }
   ];
 
   action(button) {
-    alert(`${button.label}`);
+    console.log(`${button.label}`);
   }
 
   testa() {
