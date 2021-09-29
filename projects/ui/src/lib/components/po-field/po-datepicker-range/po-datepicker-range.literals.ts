02f0b9d928ffb3c3193573b872664ad3457d38f2
gabrielibarbosa <ifc.gabrieli.barbosa@gmail.com>
2021-09-29 17:27:35 -0300

    fix(datepicker-range): corrige para permitir apenas datas válidas
    O componente estava permitindo inserir valores inválidos via digitação,
    como exemplo 31/02/2021, sendo que Fevereiro é até dia 28.
    
    Fixes DTHFUI-5127

--- projects/ui/src/lib/components/po-field/po-datepicker-range/po-datepicker-range.literals.ts
+++ projects/ui/src/lib/components/po-field/po-datepicker-range/po-datepicker-range.literals.ts
@@ -3,18 +3,22 @@ import { PoDatepickerRangeLiterals } from './interfaces/po-datepicker-range-lite
 export const poDatepickerRangeLiteralsDefault = {
   en: <PoDatepickerRangeLiterals>{
     invalidFormat: 'Date in invalid format',
-    startDateGreaterThanEndDate: 'Start date greater than end date'
+    startDateGreaterThanEndDate: 'Start date greater than end date',
+    invalidDate: 'Invalid date'
   },
   es: <PoDatepickerRangeLiterals>{
     invalidFormat: 'Fecha en formato no válido',
-    startDateGreaterThanEndDate: 'Fecha de inicio mayor que fecha final'
+    startDateGreaterThanEndDate: 'Fecha de inicio mayor que fecha final',
+    invalidDate: 'Fecha invalida'
   },
   pt: <PoDatepickerRangeLiterals>{
     invalidFormat: 'Data no formato inválido',
-    startDateGreaterThanEndDate: 'Data inicial maior que data final'
+    startDateGreaterThanEndDate: 'Data inicial maior que data final',
+    invalidDate: 'Data inválida'
   },
   ru: <PoDatepickerRangeLiterals>{
     invalidFormat: 'Дата в неверном формате',
-    startDateGreaterThanEndDate: 'Дата начала больше даты окончания'
+    startDateGreaterThanEndDate: 'Дата начала больше даты окончания',
+    invalidDate: 'Недействительная дата'
   }
 };
