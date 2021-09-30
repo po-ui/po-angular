f50542b427b4692a1b083e2df2d2bfbbc1608b23
alinelariguet <alinecbgmoreira@gmail.com>
2021-09-30 18:59:36 -0300

    feat(button): remove samples e2e

--- projects/ui/src/lib/components/po-button/po-button.component.ts
+++ projects/ui/src/lib/components/po-button/po-button.component.ts
@@ -12,15 +12,11 @@ import { PoButtonBaseComponent } from './po-button-base.component';
  * <example name="po-button-basic" title="PO Button Basic">
  *  <file name="sample-po-button-basic/sample-po-button-basic.component.html"> </file>
  *  <file name="sample-po-button-basic/sample-po-button-basic.component.ts"> </file>
- *  <file name="sample-po-button-basic/sample-po-button-basic.component.e2e-spec.ts"> </file>
- *  <file name="sample-po-button-basic/sample-po-button-basic.component.po.ts"> </file>
  * </example>
  *
  * <example name="po-button-labs" title="PO Button Labs">
  *  <file name="sample-po-button-labs/sample-po-button-labs.component.html"> </file>
  *  <file name="sample-po-button-labs/sample-po-button-labs.component.ts"> </file>
- *  <file name="sample-po-button-labs/sample-po-button-labs.component.e2e-spec.ts"> </file>
- *  <file name="sample-po-button-labs/sample-po-button-labs.component.po.ts"> </file>
  * </example>
  *
  * <example name="po-button-social-network" title="PO Button Social Network">
