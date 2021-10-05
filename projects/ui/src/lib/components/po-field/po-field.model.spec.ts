2a539ead455e8d733e5da04ea25ab247e6f16340
jribeiroo <nicolas.jnr10@outlook.com>
2021-10-05 19:10:55 -0300

    refactor(switch): reestrutura componente para utilizar classe PoField
    O componente passa a utilizar a classe PoField,
    fazendo parte da reestruturação dos componentes
    de entrada.
    
    Fixes DTHFUI-4259

---
+++ projects/ui/src/lib/components/po-field/po-field.model.spec.ts
@@ -0,0 +1,72 @@
+import { PoFieldModel } from './po-field.model';
+
+class Field extends PoFieldModel<any> {
+  onWriteValue() {}
+}
+
+describe('PoFieldModel', () => {
+  let component: Field;
+
+  beforeEach(() => {
+    component = new Field();
+  });
+
+  it('should be created', () => {
+    expect(component instanceof Field).toBeTruthy();
+  });
+
+  it('registerOnChange: should set propagateChange property', () => {
+    const fnChange = () => {};
+
+    component.registerOnChange(fnChange);
+
+    expect(component['propagateChange']).toBe(fnChange);
+  });
+
+  it('registerOnTouched: should set ounTouched property', () => {
+    const fnTouched = () => {};
+
+    component.registerOnTouched(fnTouched);
+
+    expect(component['onTouched']).toBe(fnTouched);
+  });
+
+  it(`updateModel: should call propagateChange with model param`, () => {
+    const expectedValue = 'newValue';
+    component['propagateChange'] = () => {};
+
+    const spyPropagateChange = spyOn(component, <any>'propagateChange');
+
+    component['updateModel'](expectedValue);
+
+    expect(spyPropagateChange).toHaveBeenCalledWith(expectedValue);
+  });
+
+  it(`updateModel: shouldn't call propagateChange if propagateChange is falsy`, () => {
+    const expectedValue = 'newValue';
+
+    component['propagateChange'] = undefined;
+
+    component['updateModel'](expectedValue);
+
+    expect(component['propagateChange']).toBeUndefined();
+  });
+
+  it('setDisabledState: should set disabled property', () => {
+    const isDisabled = true;
+
+    component.setDisabledState(isDisabled);
+
+    expect(component.disabled).toBe(true);
+  });
+
+  it('writeValue: should call onWriteValue with value', () => {
+    const expectedValue = false;
+
+    const spyOnWriteValue = spyOn(component, <any>'onWriteValue');
+
+    component.writeValue(expectedValue);
+
+    expect(spyOnWriteValue).toHaveBeenCalledWith(expectedValue);
+  });
+});
--- projects/ui/src/lib/components/po-field/po-switch/po-switch.component.spec.ts
+++ projects/ui/src/lib/components/po-field/po-switch/po-switch.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { configureTestSuite } from './../../../util-test/util-expect.spec';
+import { configureTestSuite, expectPropertiesValues, expectSettersMethod } from './../../../util-test/util-expect.spec';
 
 import { PoSwitchComponent } from './po-switch.component';
 import { PoSwitchLabelPosition } from './po-switch-label-position.enum';
@@ -29,88 +29,27 @@ describe('PoSwitchComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have .po-switch-container-on', () => {
-    component.switchValue = true;
-    fixture.detectChanges();
+  describe('Properties:', () => {
+    it('should be update property p-label-on', () => {
+      expectSettersMethod(component, 'labelOn', '', 'labelOn', 'true');
+      expectSettersMethod(component, 'labelOn', 'On', 'labelOn', 'On');
+    });
 
-    expect(nativeElement.querySelector('.po-switch-container-on')).toBeTruthy();
-    expect(nativeElement.querySelector('.po-switch-container-off')).toBeFalsy();
-    expect(nativeElement.querySelector('.po-switch-container-disabled')).toBeFalsy();
-  });
+    it('should be update property p-label-off', () => {
+      expectSettersMethod(component, 'labelOff', '', 'labelOff', 'false');
+      expectSettersMethod(component, 'labelOff', 'Off', 'labelOff', 'Off');
+    });
 
-  it('should have .po-switch-container-off', () => {
-    component.switchValue = false;
-    fixture.detectChanges();
-
-    expect(nativeElement.querySelector('.po-switch-container-off')).toBeTruthy();
-    expect(nativeElement.querySelector('.po-switch-container-on')).toBeFalsy();
-    expect(nativeElement.querySelector('.po-switch-container-disabled')).toBeFalsy();
-  });
-
-  it('should have .po-switch-container-disabled', () => {
-    component.switchValue = false;
-    component.disabled = true;
-    fixture.detectChanges();
-
-    expect(nativeElement.querySelector('.po-switch-container-disabled')).toBeTruthy();
-    expect(nativeElement.querySelector('.po-switch-container-on')).toBeFalsy();
-    expect(nativeElement.querySelector('.po-switch-container-off')).toBeFalsy();
-  });
-
-  it('should have .po-switch-button-on', () => {
-    component.switchValue = true;
-    fixture.detectChanges();
-
-    expect(nativeElement.querySelector('.po-icon-ok')).toBeTruthy();
-
-    expect(nativeElement.querySelector('.po-switch-button-on')).toBeTruthy();
-    expect(nativeElement.querySelector('.po-switch-button-off')).toBeFalsy();
-    expect(nativeElement.querySelector('.po-switch-button-disabled')).toBeFalsy();
-  });
-
-  it('should have .po-switch-button-off', () => {
-    component.switchValue = false;
-    fixture.detectChanges();
-
-    expect(nativeElement.querySelector('.po-icon-close')).toBeTruthy();
-
-    expect(nativeElement.querySelector('.po-switch-button-off')).toBeTruthy();
-    expect(nativeElement.querySelector('.po-switch-button-on')).toBeFalsy();
-    expect(nativeElement.querySelector('.po-switch-button-disabled')).toBeFalsy();
-  });
-
-  it('should have .po-switch-button-disabled', () => {
-    component.switchValue = true;
-    component.disabled = true;
-    fixture.detectChanges();
-
-    expect(nativeElement.querySelector('.po-switch-button-disabled')).toBeTruthy();
-    expect(nativeElement.querySelector('.po-switch-button-on')).toBeTruthy();
-    expect(nativeElement.querySelector('.po-switch-button-off')).toBeFalsy();
-  });
-
-  it('should have p-label-position = default', () => {
-    component.labelPosition = undefined;
-    fixture.detectChanges();
-
-    expect(nativeElement.querySelector('.po-switch-label-right')).toBeTruthy();
-    expect(nativeElement.querySelector('.po-switch-container-right')).toBeFalsy();
-  });
-
-  it('should have p-label-position = left', () => {
-    component.labelPosition = PoSwitchLabelPosition.Left;
-    fixture.detectChanges();
-
-    expect(nativeElement.querySelector('.po-switch-label-left')).toBeTruthy();
-    expect(nativeElement.querySelector('.po-switch-container-right')).toBeTruthy();
-  });
-
-  it('should have p-label-position = right', () => {
-    component.labelPosition = PoSwitchLabelPosition.Right;
-    fixture.detectChanges();
-
-    expect(nativeElement.querySelector('.po-switch-label-right')).toBeTruthy();
-    expect(nativeElement.querySelector('.po-switch-container-right')).toBeFalsy();
+    it('should be update property p-label-position', () => {
+      expectSettersMethod(component, 'labelPosition', '', 'labelPosition', PoSwitchLabelPosition.Right);
+      expectSettersMethod(
+        component,
+        'labelPosition',
+        PoSwitchLabelPosition.Left,
+        'labelPosition',
+        PoSwitchLabelPosition.Left
+      );
+    });
   });
 
   describe('Methods:', () => {
@@ -209,12 +148,13 @@ describe('PoSwitchComponent', () => {
     });
 
     it('onBlur: should call `onTouched` on blur', () => {
-      component.onTouched = value => {};
+      component['onTouched'] = value => {};
 
-      spyOn(component, 'onTouched');
+      spyOn(component, <any>'onTouched');
+
       component.onBlur();
 
-      expect(component.onTouched).toHaveBeenCalledWith();
+      expect(component['onTouched']).toHaveBeenCalledWith();
     });
 
     it('onBlur: shouldn´t throw error if onTouched is falsy', () => {
@@ -223,6 +163,77 @@ describe('PoSwitchComponent', () => {
       const fnError = () => component.onBlur();
 
       expect(fnError).not.toThrow();
+    });
+
+    it('changeValue: shouldn`t call `change.emit` and `updateModel` if switch value is not changed', () => {
+      component.value = true;
+
+      spyOn(component.change, 'emit');
+      spyOn(component, <any>'updateModel');
+
+      component.changeValue(true);
+      expect(component['updateModel']).not.toHaveBeenCalled();
+      expect(component.change.emit).not.toHaveBeenCalled();
+    });
+
+    it('changeValue: should call `updateModel` and `change.emit` if switch value is changed', () => {
+      component.value = true;
+
+      spyOn(component.change, 'emit');
+      spyOn(component, <any>'updateModel');
+      component.changeValue(false);
+
+      expect(component['updateModel']).toHaveBeenCalledWith(false);
+      expect(component.change.emit).toHaveBeenCalledWith(false);
+    });
+
+    it('onWriteValue: should updated value and call `markForCheck`', () => {
+      const expectedValue = false;
+
+      component.value = true;
+
+      component['changeDetector'] = <any>{ markForCheck: () => {} };
+      spyOn(component['changeDetector'], 'markForCheck');
+
+      component.onWriteValue(expectedValue);
+
+      expect(component['changeDetector'].markForCheck).toHaveBeenCalled();
+      expect(component.value).toBe(expectedValue);
+    });
+
+    it('onWriteValue: shouldn`t updated value if new value equals old value', () => {
+      const expectedValue = true;
+
+      component.value = expectedValue;
+
+      component['changeDetector'] = <any>{ markForCheck: () => {} };
+
+      spyOn(component['changeDetector'], 'markForCheck');
+
+      component.onWriteValue(expectedValue);
+
+      expect(component['changeDetector'].markForCheck).not.toHaveBeenCalled();
+      expect(component.value).toBe(expectedValue);
+    });
+
+    it('eventClick: shouldn`t call changeValue if disabled is true', () => {
+      component.disabled = true;
+
+      spyOn(component, 'changeValue');
+
+      component.eventClick();
+
+      expect(component.changeValue).not.toHaveBeenCalled();
+    });
+
+    it('eventClick: should call changeValue', () => {
+      component.disabled = false;
+
+      spyOn(component, 'changeValue');
+
+      component.eventClick();
+
+      expect(component.changeValue).toHaveBeenCalled();
     });
   });
 
@@ -239,6 +250,90 @@ describe('PoSwitchComponent', () => {
       fixture.detectChanges();
 
       expect(nativeElement.querySelector('div.po-switch-container.po-clickable[tabindex="0"]')).toBeTruthy();
+    });
+
+    it('should have .po-switch-container-on', () => {
+      component.value = true;
+      fixture.detectChanges();
+
+      expect(nativeElement.querySelector('.po-switch-container-on')).toBeTruthy();
+      expect(nativeElement.querySelector('.po-switch-container-off')).toBeFalsy();
+      expect(nativeElement.querySelector('.po-switch-container-disabled')).toBeFalsy();
+    });
+
+    it('should have .po-switch-container-off', () => {
+      component.value = false;
+      fixture.detectChanges();
+
+      expect(nativeElement.querySelector('.po-switch-container-off')).toBeTruthy();
+      expect(nativeElement.querySelector('.po-switch-container-on')).toBeFalsy();
+      expect(nativeElement.querySelector('.po-switch-container-disabled')).toBeFalsy();
+    });
+
+    it('should have .po-switch-container-disabled', () => {
+      component.value = false;
+      component.disabled = true;
+      fixture.detectChanges();
+
+      expect(nativeElement.querySelector('.po-switch-container-disabled')).toBeTruthy();
+      expect(nativeElement.querySelector('.po-switch-container-on')).toBeFalsy();
+      expect(nativeElement.querySelector('.po-switch-container-off')).toBeFalsy();
+    });
+
+    it('should have .po-switch-button-on', () => {
+      component.value = true;
+      fixture.detectChanges();
+
+      expect(nativeElement.querySelector('.po-icon-ok')).toBeTruthy();
+
+      expect(nativeElement.querySelector('.po-switch-button-on')).toBeTruthy();
+      expect(nativeElement.querySelector('.po-switch-button-off')).toBeFalsy();
+      expect(nativeElement.querySelector('.po-switch-button-disabled')).toBeFalsy();
+    });
+
+    it('should have .po-switch-button-off', () => {
+      component.value = false;
+      fixture.detectChanges();
+
+      expect(nativeElement.querySelector('.po-icon-close')).toBeTruthy();
+
+      expect(nativeElement.querySelector('.po-switch-button-off')).toBeTruthy();
+      expect(nativeElement.querySelector('.po-switch-button-on')).toBeFalsy();
+      expect(nativeElement.querySelector('.po-switch-button-disabled')).toBeFalsy();
+    });
+
+    it('should have .po-switch-button-disabled', () => {
+      component.value = true;
+      component.disabled = true;
+      fixture.detectChanges();
+
+      expect(nativeElement.querySelector('.po-switch-button-disabled')).toBeTruthy();
+      expect(nativeElement.querySelector('.po-switch-button-on')).toBeTruthy();
+      expect(nativeElement.querySelector('.po-switch-button-off')).toBeFalsy();
+    });
+
+    it('should have p-label-position = default', () => {
+      component['_labelPosition'] = undefined;
+      fixture.detectChanges();
+
+      expect(nativeElement.querySelector('.po-switch-label-right')).toBeTruthy();
+      expect(nativeElement.querySelector('.po-switch-container-right')).toBeFalsy();
+    });
+
+    it('should have p-label-position = left', () => {
+      component.labelPosition = PoSwitchLabelPosition.Left;
+      fixture.detectChanges();
+
+      expect(nativeElement.querySelector('.po-switch-label-left')).toBeTruthy();
+      expect(nativeElement.querySelector('.po-switch-container-right')).toBeTruthy();
+    });
+
+    it('should have p-label-position = right', () => {
+      component.labelPosition = PoSwitchLabelPosition.Right;
+      fixture.detectChanges();
+
+      expect(nativeElement.querySelector('.po-switch-label-right')).toBeTruthy();
+      expect(nativeElement.querySelector('.po-switch-container-right')).toBeFalsy();
     });
   });
 });
