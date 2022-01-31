d15e68bf5f8bc5f105475016dece94f964ca7fa3
Wanderley Santos Teixeira <wsteixeira@yahoo.com.br>
2022-01-31 20:04:39 -0300

    feat(page-dynamic-table): adciona p-infinite-scroll e p-height
    Adiciona as propriedades p-infinite-scroll e p-height que serão repassadas para o componente po-table, onde já são suportadas.
    
    #1092

---
+++ projects/templates/src/lib/components/po-page-dynamic-table/samples/sample-po-page-dynamic-table-people/sample-po-page-dynamic-table-people.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sample-po-page-dynamic-table-people',
+  templateUrl: './sample-po-page-dynamic-table-people.component.html'
+})
+export class SamplePoPageDynamicTablePeopleComponent {
+  readonly serviceApi = 'https://po-sample-api.herokuapp.com/v1/people';
+
+  readonly fields: Array<any> = [
+    { property: 'id', key: true, visible: false },
+    { property: 'name', label: 'Name' },
+    { property: 'genre', label: 'Genre', sortable: false },
+    { property: 'city', label: 'City' }
+  ];
+
+  constructor() {}
+}
