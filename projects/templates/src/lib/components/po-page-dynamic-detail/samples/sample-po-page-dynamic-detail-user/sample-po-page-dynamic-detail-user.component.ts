a31e2dfa3484254c6faa95a4bbea2115dfb839a7
Wanderley Santos Teixeira <wsteixeira@yahoo.com.br>
2022-02-04 08:15:36 -0300

    test(page-dynamic-detail): melhoria no exemplo
    O exemplo do estava apresentando o erro 'Registro não encontrado'.
    
    Fixes #1175

--- projects/templates/src/lib/components/po-page-dynamic-detail/samples/sample-po-page-dynamic-detail-user/sample-po-page-dynamic-detail-user.component.ts
+++ projects/templates/src/lib/components/po-page-dynamic-detail/samples/sample-po-page-dynamic-detail-user/sample-po-page-dynamic-detail-user.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck, ViewChild } from '@angular/core';
 
 import { PoBreadcrumb } from '@po-ui/ng-components';
-import { PoPageDynamicDetailActions, PoPageDynamicDetailField } from '@po-ui/ng-templates';
+import {
+  PoPageDynamicDetailActions,
+  PoPageDynamicDetailComponent,
+  PoPageDynamicDetailField
+} from '@po-ui/ng-templates';
 
 @Component({
   selector: 'sample-po-page-dynamic-detail-user',
   templateUrl: './sample-po-page-dynamic-detail-user.component.html'
 })
-export class SamplePoPageDynamicDetailUserComponent {
+export class SamplePoPageDynamicDetailUserComponent implements DoCheck {
+  @ViewChild(PoPageDynamicDetailComponent, { static: true }) poPageDynamicDetail: PoPageDynamicDetailComponent;
+
   public readonly serviceApi = 'https://po-sample-api.herokuapp.com/v1/people';
 
   public readonly actions: PoPageDynamicDetailActions = {
@@ -39,4 +45,70 @@ export class SamplePoPageDynamicDetailUserComponent {
     { property: 'city' },
     { property: 'country' }
   ];
+
+  public readonly model = {
+    id: '0148093543698',
+    name: 'Ronaldo Nascimento Filho',
+    birthdate: '1980-11-01T00:00:00-00:00',
+    genre: 'male',
+    city: '3550308',
+    status: 'active',
+    nickname: 'ronaldo.filho',
+    email: 'ronaldo.filho@gmail.com',
+    nationality: 'Brazilian',
+    birthPlace: 'São Paulo',
+    graduation: 'College',
+    father: 'Papai',
+    mother: 'Mamãe',
+    street: 'Santos Dumont',
+    country: 'Brasil',
+    genreDescription: 'Masculino',
+    statusDescription: 'Ativo',
+    cityName: 'São Paulo',
+    state: 'São Paulo',
+    uf: 'SP',
+    dependents: [
+      {
+        id: 109481,
+        name: 'Maria',
+        age: '10',
+        related: 'Daughter',
+        birthdate: '2008-12-10'
+      },
+      {
+        id: 109482,
+        name: 'Joana',
+        age: '12',
+        related: 'Daughter',
+        birthdate: '2008-12-10'
+      },
+      {
+        id: 109483,
+        name: 'Pedro',
+        age: '13',
+        related: 'Son',
+        birthdate: '2008-12-10'
+      },
+      {
+        id: 109484,
+        name: 'Paulo',
+        age: '15',
+        related: 'Son',
+        birthdate: '2008-12-10'
+      },
+      {
+        id: 109485,
+        name: 'José',
+        age: '19',
+        related: 'Son',
+        birthdate: '2008-12-10'
+      }
+    ]
+  };
+
+  constructor() {}
+
+  ngDoCheck() {
+    this.poPageDynamicDetail.model = this.model;
+  }
 }
