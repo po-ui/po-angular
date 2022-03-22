28c771f0e8ad289aa3988b98ee1fa147efc84f0a
Nicole Oliveira <nicoleoliveira1995@gmail.com>
2022-03-22 18:05:01 -0300

    feat: demo com os tokens

--- projects/app/src/app/app.component.ts
+++ projects/app/src/app/app.component.ts
@@ -1,7 +1,44 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { PoButtonComponent } from '../../../ui/src/lib';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent implements AfterViewInit {
+  @ViewChild('button') button: PoButtonComponent;
+
+  form = this.formBuilder.group({
+    borderRadius: [null],
+    fontSize: [null],
+    color: [null]
+  });
+
+  private readonly formPropertyDict = {
+    borderRadius: '--border-radius',
+    fontSize: '--font-size',
+    color: '--color'
+  };
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngAfterViewInit(): void {
+    this.form.valueChanges.subscribe(changes => this.checkChanges(changes));
+  }
+
+  private checkChanges(changes: { [key: string]: string }): void {
+    Object.keys(changes).forEach((fieldName: string) => {
+      console.log(changes[fieldName]);
+      if (fieldName.includes('color')) {
+        this.button.buttonElement.nativeElement.style.setProperty(this.formPropertyDict[fieldName], changes[fieldName]);
+      }
+      if (changes[fieldName]) {
+        this.button.buttonElement.nativeElement.style.setProperty(
+          this.formPropertyDict[fieldName],
+          `var(--${changes[fieldName]})`
+        );
+      }
+    });
+  }
+}
