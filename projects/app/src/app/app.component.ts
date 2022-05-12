ca5e601afed2de8e3eb0248380df57dd1e8306c9
jcorrea97 <jroberto.cr1@gmail.com>
2022-05-12 14:27:38 -0300

    feat(spike-theme-totvs): aplica global-tokens

--- projects/app/src/app/app.component.ts
+++ projects/app/src/app/app.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+
+import {
+  PoModalComponent,
+  PoNotification,
+  PoNotificationService,
+  PoToasterOrientation,
+  PoToasterType,
+  PoRadioGroupOption
+} from '../../../ui/src/lib';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {}
