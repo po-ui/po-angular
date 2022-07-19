3d5614e6e55f676c0a2d87fd592914ea224f5770
Wanderley Santos Teixeira <wsteixeira@yahoo.com.br>
2022-07-19 16:16:00 -0300

    fix(language): adiciona limpeza das variáveis do `localStorage`
    Foi adicionado a limpeza das variáveis do `localStorage`:
    - `PO_DEFAULT_LANGUAGE`
    - `PO_USER_LOCALE`
    
    Estas variáveis são populadas pelas funções:
    - `languageDefault`.
    - `setLanguage`.
    
    A limpeza é necessária quando se deixa de usar as funções acima,
    evitando uso de valores indevidos.
    
    Fixes #1331

--- projects/ui/src/lib/services/po-language/po-language.service.ts
+++ projects/ui/src/lib/services/po-language/po-language.service.ts
@@ -8,6 +8,9 @@ import {
   poLocaleThousandSeparatorList
 } from './po-language.constant';
 
+localStorage.removeItem('PO_DEFAULT_LANGUAGE');
+localStorage.removeItem('PO_USER_LOCALE');
+
 const poDefaultLanguage = 'PO_DEFAULT_LANGUAGE';
 const poLocaleKey = 'PO_USER_LOCALE';
 
