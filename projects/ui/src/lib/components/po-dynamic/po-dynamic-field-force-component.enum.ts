b1ac8fd1c950476bd25b11efdfebdd82b2a952d5
alinelariguet <alinecbgmoreira@gmail.com>
2022-07-25 11:03:09 -0300

    fix(dynamic-form): exporta ForceOptionComponentEnum

--- projects/ui/src/lib/components/po-dynamic/po-dynamic-field-force-component.enum.ts
+++ projects/ui/src/lib/components/po-dynamic/po-dynamic-field-force-component.enum.ts
@@ -1,4 +1,14 @@
+/**
+ * @usedBy PoDynamicFormComponent
+ *
+ * @description
+ *
+ * Enum para definição do tipo de componente a ser renderizado.
+ */
 export enum ForceOptionComponentEnum {
+  /** Força a renderização de um po-radio-group independente da quantidade do opções */
   radioGroup = 'radioGroup',
+
+  /** Força a renderização de um po-select independente da quantidade do opções */
   select = 'select'
 }
