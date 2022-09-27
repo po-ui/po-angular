b4c91271f60b3651305f030283ee6595d48d243b
Wanderley Santos Teixeira <wsteixeira@yahoo.com.br>
2022-09-27 19:19:58 -0300

    feat(page-dynamic-edit): adiciona parâmetro `p-literals`
    Foi adicionado o parâmetro `p-literals` para customizar rótulo dos
    botões e texto das mensagens
    
    Fixes #1321

---
+++ projects/templates/src/lib/components/po-page-dynamic-edit/interfaces/po-page-dynamic-edit-literals.interface.ts
@@ -0,0 +1,71 @@
+/**
+ * @usedBy PoPageDynamicEditComponent
+ *
+ * @description
+ *
+ * Interface para definição das literais usadas no `po-page-dynamic-edit`.
+ */
+export interface PoPageDynamicEditLiterals {
+  /**
+   * @description
+   *
+   * Texto exibido na mensagem de cancelamento da inclusão/edição.
+   */
+  cancelConfirmMessage?: string;
+
+  /**
+   * @description
+   *
+   * Rótulo exibido no botão `Novo`.
+   */
+  detailActionNew?: string;
+
+  /**
+   * @description
+   *
+   * Rótulo exibido no botão `Cancelar`.
+   */
+  pageActionCancel?: string;
+
+  /**
+   * @description
+   *
+   * Rótulo exibido no botão `Salvar`.
+   */
+  pageActionSave?: string;
+
+  /**
+   * @description
+   *
+   * Rótulo exibido no botão `Salvar e novo`.
+   */
+  pageActionSaveNew?: string;
+
+  /**
+   * @description
+   *
+   * Texto exibido para resgistro não encontrado.
+   */
+  registerNotFound?: string;
+
+  /**
+   * @description
+   *
+   * Texto exibido para recurso salvo com sucesso.
+   */
+  saveNotificationSuccessSave?: string;
+
+  /**
+   * @description
+   *
+   * Texto exibido para recurso atualizado com sucesso.
+   */
+  saveNotificationSuccessUpdate?: string;
+
+  /**
+   * @description
+   *
+   * Texto exibido para adivertência de formulário preenchido de forma incorreta.
+   */
+  saveNotificationWarning?: string;
+}
