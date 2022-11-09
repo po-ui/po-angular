7190705afcc54d5e0b800e52535bf58335d27a23
Bruno Romeiro <bruno.romeiro@gmail.com>
2022-11-09 15:57:50 -0300

    docs(table): adiciona novo sample Heroes
    Adiciona novo sample utilizando o método RemoveItem da Table
    
    Fixes DTHFUI-6695

---
+++ projects/ui/src/lib/components/po-table/samples/sample-po-table-heroes/sample-po-table-heroes.component.ts
@@ -0,0 +1,51 @@
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { PoTableColumn, PoTableComponent } from '@po-ui/ng-components';
+
+import { SamplePoTableHeroesService } from './sample-po-table-heroes.service';
+
+@Component({
+  selector: 'sample-po-table-heroes',
+  templateUrl: './sample-po-table-heroes.component.html',
+  providers: [SamplePoTableHeroesService]
+})
+export class SamplePoTableHeroesComponent implements OnInit {
+  @ViewChild('POItemsOri', { static: true }) poItemsOri: PoTableComponent;
+  @ViewChild('POItemsSelected', { static: true }) poItemsSelected: PoTableComponent;
+
+  items: Array<any> = [];
+  itemsSelected: Array<any> = [];
+  columns: Array<PoTableColumn>;
+
+  constructor(private service: SamplePoTableHeroesService) {}
+
+  ngOnInit(): void {
+    this.getColumns();
+    this.getItems();
+  }
+
+  getColumns(): void {
+    this.columns = this.service.getColumns();
+  }
+
+  getItems(): void {
+    this.service.getItems().subscribe({
+      next: res => (this.items = res),
+      error: err => console.error(err)
+    });
+  }
+
+  changeOptions(event, type): void {
+    if (type === 'new') {
+      this.itemsSelected.push({
+        id: event.id,
+        label: event.label,
+        email: event.email
+      });
+      this.itemsSelected = [...this.itemsSelected];
+    } else {
+      const index = this.itemsSelected.findIndex(el => el.id === event.id);
+      this.poItemsSelected.removeItem(index);
+      this.itemsSelected = [...this.poItemsSelected.items];
+    }
+  }
+}
