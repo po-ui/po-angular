059d5e121d5601038af959944450112fc2f7cf4b
jribeiroo <nicolas.jnr10@outlook.com>
2022-12-01 09:57:12 -0300

    feat(fields): implementa definições do AnimaliaDS
    Implementa definições de estilo e acessibilidade definidos pelo AnimaliaDS
    
    Fixes DTHFUI-6804

--- projects/ui/src/lib/components/po-field/po-field-container/po-field-container-bottom/po-field-container-bottom.component.spec.ts
+++ projects/ui/src/lib/components/po-field/po-field-container/po-field-container-bottom/po-field-container-bottom.component.spec.ts
@@ -27,15 +27,4 @@ describe('PoFieldContainerBottomComponent', () => {
     expect(fixture.debugElement.nativeElement.querySelector('.po-field-container-bottom-text-error')).toBeNull();
     expect(fixture.debugElement.nativeElement.querySelector('.po-field-container-icon-error')).toBeNull();
   });
-
-  it('should show error pattern if has error pattern', () => {
-    component.errorPattern = 'MENSAGEM DE ERRO';
-    fixture.detectChanges();
-    const content = fixture.debugElement.nativeElement
-      .querySelector('.po-field-container-bottom-text-error')
-      .innerHTML.toString();
-
-    expect(content.includes('MENSAGEM DE ERRO')).toBeTruthy();
-    expect(fixture.debugElement.nativeElement.querySelector('.po-field-container-icon-error')).not.toBeNull();
-  });
 });
