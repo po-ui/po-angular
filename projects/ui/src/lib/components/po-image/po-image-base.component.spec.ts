b5a94a6594021f0dc095f3323611fde92b8b18f5
jribeiroo <nicolas.jnr10@outlook.com>
2023-01-27 10:00:32 -0300

    feat(image): implementa diretiva NgOptimizedImage
    Implementa diretiva `NgOptimizedImage` fornecendo melhor desempenho
    para carregamento de imagens.
    
    fixes DTHFUI-7013

--- projects/ui/src/lib/components/po-image/po-image-base.component.spec.ts
+++ projects/ui/src/lib/components/po-image/po-image-base.component.spec.ts
@@ -6,4 +6,10 @@ describe('PoImageBaseComponent:', () => {
   it('should be created', () => {
     expect(component instanceof PoImageBaseComponent).toBeTruthy();
   });
+
+  it('should isBase64 to true for valid image base64 string', () => {
+    const validBase64 = 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD';
+    component.source = validBase64;
+    expect(component.isBase64).toBe(true);
+  });
 });
