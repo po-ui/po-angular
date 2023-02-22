d45fd4fa7caa70529cac4140cbb0258a552aa85e
Rafael Lourenço Marques <rafael.lourenco@totvs.com.br>
2023-02-22 12:35:35 -0300

    feat(logo): novo componente po-logo
    Cria componente de logo, seguindo as regras definidas do AnimaliaDS.
    
    Fixes DTHFUI-6808

--- projects/ui/src/lib/components/components.module.ts
+++ projects/ui/src/lib/components/components.module.ts
@@ -1,10 +1,11 @@
+// Angular modules
 import { NgModule } from '@angular/core';
-
+// Po components modules
 import { PoAccordionModule } from './po-accordion/po-accordion.module';
 import { PoAvatarModule } from './po-avatar/po-avatar.module';
 import { PoBreadcrumbModule } from './po-breadcrumb/po-breadcrumb.module';
-import { PoButtonModule } from './po-button/po-button.module';
 import { PoButtonGroupModule } from './po-button-group/po-button-group.module';
+import { PoButtonModule } from './po-button/po-button.module';
 import { PoCalendarModule } from './po-calendar/po-calendar.module';
 import { PoChartModule } from './po-chart/po-chart.module';
 import { PoContainerModule } from './po-container/po-container.module';
@@ -16,11 +17,17 @@ import { PoDynamicModule } from './po-dynamic/po-dynamic.module';
 import { PoFieldModule } from './po-field/po-field.module';
 import { PoGaugeModule } from './po-gauge/po-gauge.module';
 import { PoGridModule } from './po-grid/po-grid.module';
+import { PoIconModule } from './po-icon/po-icon.module';
+import { PoImageModule } from './po-image/po-image.module';
 import { PoInfoModule } from './po-info/po-info.module';
+import { PoLabelModule } from './po-label/po-label.module';
+import { PoLinkModule } from './po-link/po-link.module';
 import { PoListViewModule } from './po-list-view/po-list-view.module';
+import { PoListBoxModule } from './po-listbox/po-listbox.module';
 import { PoLoadingModule } from './po-loading/po-loading.module';
-import { PoMenuModule } from './po-menu/po-menu.module';
+import { PoLogoModule } from './po-logo/po-logo.module';
 import { PoMenuPanelModule } from './po-menu-panel/po-menu-panel.module';
+import { PoMenuModule } from './po-menu/po-menu.module';
 import { PoModalModule } from './po-modal/po-modal.module';
 import { PoNavbarModule } from './po-navbar/po-navbar.module';
 import { PoPageModule } from './po-page/po-page.module';
@@ -35,11 +42,6 @@ import { PoTagModule } from './po-tag/po-tag.module';
 import { PoToolbarModule } from './po-toolbar/po-toolbar.module';
 import { PoTreeViewModule } from './po-tree-view/po-tree-view.module';
 import { PoWidgetModule } from './po-widget/po-widget.module';
-import { PoIconModule } from './po-icon/po-icon.module';
-import { PoLinkModule } from './po-link/po-link.module';
-import { PoLabelModule } from './po-label/po-label.module';
-import { PoImageModule } from './po-image/po-image.module';
-import { PoListBoxModule } from './po-listbox/po-listbox.module';
 
 @NgModule({
   imports: [
@@ -64,6 +66,7 @@ import { PoListBoxModule } from './po-listbox/po-listbox.module';
     PoListViewModule,
     PoListBoxModule,
     PoLoadingModule,
+    PoLogoModule,
     PoMenuModule,
     PoMenuPanelModule,
     PoModalModule,
@@ -106,6 +109,7 @@ import { PoListBoxModule } from './po-listbox/po-listbox.module';
     PoListViewModule,
     PoListBoxModule,
     PoLoadingModule,
+    PoLogoModule,
     PoMenuModule,
     PoMenuPanelModule,
     PoModalModule,
