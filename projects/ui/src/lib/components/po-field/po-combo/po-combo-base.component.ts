b0ff6fff78c187c5fb04d2c12a8707465c20cbe0
jcorrea97 <jroberto.cr1@gmail.com>
2023-06-06 10:59:04 -0300

    feat(combo): cria nova propriedade `p-remove-initial-filter`
    Cria nova propriedade `p-remove-initial-filter` para remover a filtragem de itens no primeiro clique do combo.
    
    fixes DTHFUI-7339

--- projects/ui/src/lib/components/po-field/po-combo/po-combo-base.component.ts
+++ projects/ui/src/lib/components/po-field/po-combo/po-combo-base.component.ts
@@ -207,6 +207,20 @@ export abstract class PoComboBaseComponent implements ControlValueAccessor, OnIn
    *
    * @description
    *
+   * Define que o filtro no primeiro clique será removido.
+   *
+   * > Caso o combo tenha um valor padrão de inicialização, o primeiro clique
+   * no componente retornará todos os itens da lista e não apenas o item inicialiazado.
+   *
+   * @default `false`
+   */
+  @Input('p-remove-initial-filter') removeInitialFilter: boolean = false;
+
+  /**
+   * @optional
+   *
+   * @description
+   *
    * Deve ser informada uma função que será disparada quando houver alterações no ngModel. A função receberá como argumento o model modificado.
    *
    * > Pode-se optar pelo recebimento do objeto selecionado ao invés do model através da propriedade `p-emit-object-value`.
@@ -742,8 +756,12 @@ export abstract class PoComboBaseComponent implements ControlValueAccessor, OnIn
   updateComboList(options?: Array<any>) {
     const copyOptions = options || [...this.comboOptionsList];
 
-    const newOptions =
-      !options && !this.infiniteScroll && this.selectedValue ? [{ ...this.selectedOption }] : copyOptions;
+    let newOptions;
+    if (this.removeInitialFilter) {
+      newOptions = copyOptions;
+    } else {
+      newOptions = !options && !this.infiniteScroll && this.selectedValue ? [{ ...this.selectedOption }] : copyOptions;
+    }
 
     this.visibleOptions = newOptions;
 
@@ -823,6 +841,7 @@ export abstract class PoComboBaseComponent implements ControlValueAccessor, OnIn
       const option = this.getOptionFromValue(value, this.comboOptionsList);
       this.updateSelectedValue(option);
       this.updateComboList();
+      this.removeInitialFilter = false;
       return;
     }
 
