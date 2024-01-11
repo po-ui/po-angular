e8ab1a7d5c7fa475f1f3c19182d17dacd3747b6e
jcorrea97 <jroberto.cr1@gmail.com>
2024-01-11 10:56:49 -0300

    feat(disclaimer-group): reutiliza `po-tag` no componente
    Reutiliza `po-tag` no componente `disclaimer-group` com nova definição de acessibilidade
    
    fixes DTHFUI-7945

--- projects/ui/src/lib/components/po-disclaimer-group/po-disclaimer-group.component.ts
+++ projects/ui/src/lib/components/po-disclaimer-group/po-disclaimer-group.component.ts
@@ -1,8 +1,20 @@
-import { Component, IterableDiffers, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  IterableDiffers,
+  OnChanges,
+  SimpleChanges,
+  inject
+} from '@angular/core';
 
 import { PoLanguageService } from '../../services/po-language/po-language.service';
 
+import { Subscription, fromEvent } from 'rxjs';
 import { PoDisclaimerGroupBaseComponent } from './po-disclaimer-group-base.component';
+import { PoDisclaimer } from '../po-disclaimer/po-disclaimer.interface';
 
 /**
  * @docsExtends PoDisclaimerGroupBaseComponent
@@ -31,12 +43,134 @@ import { PoDisclaimerGroupBaseComponent } from './po-disclaimer-group-base.compo
   templateUrl: './po-disclaimer-group.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PoDisclaimerGroupComponent extends PoDisclaimerGroupBaseComponent {
+export class PoDisclaimerGroupComponent extends PoDisclaimerGroupBaseComponent implements AfterViewInit, OnChanges {
+  private subscription: Subscription = new Subscription();
+
+  private el = inject(ElementRef);
+
   constructor(
     differs: IterableDiffers,
     languageService: PoLanguageService,
     protected changeDetector: ChangeDetectorRef
   ) {
     super(differs, languageService, changeDetector);
+  }
+
+  ngAfterViewInit(): void {
+    this.handleKeyboardNavigationTag();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.disclaimers) {
+      setTimeout(() => {
+        this.handleKeyboardNavigationTag();
+      });
+    }
+  }
+
+  onCloseAction(disclaimer: PoDisclaimer, event?) {
+    const index = this.disclaimers.findIndex(option => option.value === disclaimer.value);
+
+    this.removeDisclaimer(disclaimer);
+
+    this.emitChangeDisclaimers();
+    this.remove.emit({
+      removedDisclaimer: { ...disclaimer },
+      currentDisclaimers: [...this.disclaimers]
+    });
+
+    setTimeout(() => {
+      this.focusOnNextTag(index, event);
+    }, 300);
+  }
+
+  focusOnNextTag(indexClosed: number, clickOrEnter: string) {
+    if (clickOrEnter === 'enter') {
+      const tagRemoveElements: NodeListOf<Element> = this.el.nativeElement.querySelectorAll('.po-tag-remove');
+      indexClosed = indexClosed || indexClosed === 0 ? indexClosed : tagRemoveElements.length;
+      this.focusOnRemoveTag(tagRemoveElements, indexClosed);
+    } else {
+      indexClosed = 0;
+    }
+    this.handleKeyboardNavigationTag(indexClosed);
+  }
+
+  handleKeyboardNavigationTag(initialIndex = 0) {
+    this.subscription.unsubscribe();
+    this.subscription = new Subscription();
+    const tagRemoveElements: NodeListOf<Element> = this.el.nativeElement.querySelectorAll('.po-tag-remove');
+    this.initializeTagRemoveElements(tagRemoveElements, initialIndex);
+  }
+
+  private handleArrowLeft(tagRemoveElements, index) {
+    if (index > 0) {
+      this.setTabIndex(tagRemoveElements[index], -1);
+      tagRemoveElements[index - 1].focus();
+      this.setTabIndex(tagRemoveElements[index - 1], 0);
+    }
+  }
+
+  private handleArrowRight(tagRemoveElements, index) {
+    if (index < tagRemoveElements.length - 1) {
+      this.setTabIndex(tagRemoveElements[index], -1);
+      tagRemoveElements[index + 1].focus();
+      this.setTabIndex(tagRemoveElements[index + 1], 0);
+    }
+  }
+
+  private setTabIndex(element, tabIndex) {
+    element.setAttribute('tabindex', tabIndex);
+  }
+
+  private initializeTagRemoveElements(tagRemoveElements, initialIndex) {
+    tagRemoveElements.forEach((tagRemoveElement, index) => {
+      if (index === initialIndex) {
+        this.setTabIndex(tagRemoveElements[initialIndex], 0);
+      } else if (tagRemoveElements.length === initialIndex) {
+        this.setTabIndex(tagRemoveElements[initialIndex - 1], 0);
+      } else {
+        this.setTabIndex(tagRemoveElement, -1);
+      }
+
+      this.subscription.add(
+        fromEvent(tagRemoveElement, 'keydown').subscribe((event: KeyboardEvent) => {
+          this.handleKeyDown(event, tagRemoveElements, index);
+        })
+      );
+
+      if (index !== 0) {
+        this.subscription.add(
+          fromEvent(tagRemoveElements, 'blur').subscribe(() => {
+            this.setTabIndex(tagRemoveElements[index], -1);
+            this.setTabIndex(tagRemoveElements[0], 0);
+          })
+        );
+      }
+    });
+  }
+
+  private handleKeyDown(event: KeyboardEvent, tagRemoveElements, index: number) {
+    const KEY_SPACE = 'Space';
+    const KEY_ARROW_LEFT = 'ArrowLeft';
+    const KEY_ARROW_RIGHT = 'ArrowRight';
+
+    if (event.code === KEY_SPACE) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    if (event.key === KEY_ARROW_LEFT) {
+      this.handleArrowLeft(tagRemoveElements, index);
+    } else if (event.key === KEY_ARROW_RIGHT) {
+      this.handleArrowRight(tagRemoveElements, index);
+    }
+  }
+
+  private focusOnRemoveTag(tag: any, indexClosed: number) {
+    if (tag.length === indexClosed) {
+      tag[indexClosed - 1]?.focus();
+    } else {
+      tag[indexClosed]?.focus();
+    }
   }
 }
