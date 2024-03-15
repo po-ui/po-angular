1758cc0feff7a6ba3f313387d27ae63758a40530
felipepetuco <felipe.petuco@gmail.com>
2024-03-15 16:10:46 -0300

    fix(page-slide): ajusta regra do foco no elemento

--- projects/ui/src/lib/components/po-page/po-page-slide/po-page-slide.component.ts
+++ projects/ui/src/lib/components/po-page/po-page-slide/po-page-slide.component.ts
@@ -118,7 +118,8 @@ export class PoPageSlideComponent extends PoPageSlideBaseComponent {
       this.firstElement.focus();
     } else {
       const elements = getFocusableElements(this.pageContent.nativeElement);
-      const element = elements[1] || this.pageContent.nativeElement;
+      /* istanbul ignore next */
+      const element = elements[0] || this.pageContent.nativeElement;
       element.focus();
     }
   }
