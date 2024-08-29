e995ed4460b362a97d9fc54e5221ea575470fd00
ana.espejo <ana.espejo@totvs.com.br>
2024-08-29 18:59:32 -0300

    test: ajusta erros para funcionamento da esteira
    Ajusta erros nos testes para correto funcionamento da esteira.
    
    Fixes DTHFUI-9761

---
+++ projects/ui/src/lib/util-test/util-setup.spec.ts
@@ -0,0 +1,8 @@
+import { TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+
+beforeEach(async () => {
+  TestBed.configureTestingModule({
+    schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA]
+  });
+});
