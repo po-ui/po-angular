7dae7c68971a0af364607ca0f6909d37aa4ac2df
Pedro Domingues <pehdomingues96@gmail.com>
2024-09-11 19:23:53 -0300

    feat(sonar): habilita análise Sonar Totvs

--- scripts/sonar.js
+++ scripts/sonar.js
@@ -1,26 +1,56 @@
 const { task } = require('gulp');
-const sonarqubeScanner = require('sonarqube-scanner');
+const scanner = require('sonarqube-scanner').default;
 const argv = require('yargs').argv;
 
-task('sonarqube', callback => {
-  const token = argv.token || '';
-  const url = argv.url || '';
-  const projectKey = argv.projectKey || '';
+const validateArgument = (value, name) => {
+  if (!value) {
+    throw new Error(`O argumento --${name} é obrigatório.`);
+  }
+  return value;
+};
 
-  sonarqubeScanner(
-    {
-      serverUrl: url,
-      token: token,
-      options: {
-        'sonar.projectKey': projectKey,
-        'sonar.projectName': 'PO UI',
-        'sonar.sources': 'projects',
-        'sonar.exclusions':
-          '**/node_modules/**,**/*.spec.ts,projects/app/**,projects/portal/**,**/samples/**,**/karma.conf.js,**/*-literals.ts,**/schematics/**',
-        'sonar.typescript.lcov.reportPaths':
-          'coverage/code-editor/lcov.info,coverage/storage/lcov.info,coverage/sync/lcov.info,coverage/templates/lcov.info,coverage/ui/lcov.info'
-      }
-    },
-    callback
-  );
+task('sonarqube', callback => {
+  try {
+    const token = validateArgument(argv.token, 'token');
+    const url = validateArgument(argv.url, 'url');
+    const projectKey = validateArgument(argv.projectKey, 'projectKey');
+
+    const exclusions = [
+      '**/node_modules/**',
+      '**/*.spec.ts',
+      'projects/app/**',
+      'projects/portal/**',
+      '**/samples/**',
+      '**/karma.conf.js',
+      '**/*-literals.ts',
+      '**/schematics/**'
+    ].join(',');
+
+    const lcovReportPaths = [
+      'coverage/code-editor/lcov.info',
+      'coverage/storage/lcov.info',
+      'coverage/sync/lcov.info',
+      'coverage/templates/lcov.info',
+      'coverage/ui/lcov.info'
+    ].join(',');
+
+    scanner(
+      {
+        serverUrl: url,
+        token: token,
+        options: {
+          'sonar.projectKey': projectKey,
+          'sonar.login': token,
+          'sonar.projectName': projectKey,
+          'sonar.sources': 'projects',
+          'sonar.exclusions': exclusions,
+          'sonar.typescript.lcov.reportPaths': lcovReportPaths
+        }
+      },
+      callback
+    );
+  } catch (error) {
+    console.error(error.message);
+    callback(error);
+  }
 });
