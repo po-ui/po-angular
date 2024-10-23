122c4ba679ab0eddf2268e1c4035582e0ce8373a
jcorrea97 <jroberto.cr1@gmail.com>
2024-10-23 10:11:03 -0300

    fix(accordion): corrige texto grande no componente
    Corrige texto estourando a tela no accordion
    
    fixes DTHFUI-9636

--- projects/ui/src/lib/components/po-accordion/po-accordion-manager/po-accordion-manager.component.ts
+++ projects/ui/src/lib/components/po-accordion/po-accordion-manager/po-accordion-manager.component.ts
@@ -1,18 +1,52 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  EventEmitter,
+  inject,
+  Input,
+  OnChanges,
+  Output,
+  SimpleChanges,
+  ViewChild
+} from '@angular/core';
 import { PoAccordionLiterals } from '../interfaces/po-accordion-literals.interface';
 
 @Component({
   selector: 'po-accordion-manager',
   templateUrl: 'po-accordion-manager.component.html'
 })
-export class PoAccordionManagerComponent {
+export class PoAccordionManagerComponent implements OnChanges {
+  labelValue: string = '';
+  changeDetector = inject(ChangeDetectorRef);
+
+  @ViewChild('accordionHeaderButtonManagerElement', { read: ElementRef, static: true }) accordionElement: ElementRef;
+  @ViewChild('accordionHeaderManagerElement', { read: ElementRef, static: true }) accordionHeaderElement: ElementRef;
+
   @Input('p-expanded-all-items') expandedAllItems: boolean = false;
 
   @Input('p-literals') literals: PoAccordionLiterals;
 
   @Output('p-click') clickManager = new EventEmitter<boolean>();
 
+  ngOnChanges(changes: SimpleChanges): void {
+    this.labelValue = changes.expandedAllItems.currentValue
+      ? this.literals.closeAllItems
+      : this.literals.expandAllItems;
+    this.changeDetector.detectChanges();
+  }
+
   onClick() {
     this.clickManager.emit();
+  }
+
+  getTooltip() {
+    const widthContainer = this.accordionElement.nativeElement.offsetWidth - 69;
+    const widthHeaderElement = this.accordionHeaderElement.nativeElement.offsetWidth;
+
+    if (widthHeaderElement >= widthContainer) {
+      return this.labelValue;
+    }
+    return null;
   }
 }
