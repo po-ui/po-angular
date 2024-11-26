5c4756521c554a5740cdd495dad4d44c0110821e
felipepetuco <felipe.petuco@gmail.com>
2024-11-26 17:00:05 -0300

    feat(combo): considera p-remove-initial-filter junto ao p-filter-service

--- projects/templates/src/lib/components/po-page-dynamic-search/po-page-dynamic-search.component.ts
+++ projects/templates/src/lib/components/po-page-dynamic-search/po-page-dynamic-search.component.ts
@@ -5,7 +5,8 @@ import {
   OnDestroy,
   ChangeDetectorRef,
   AfterViewInit,
-  SimpleChanges
+  SimpleChanges,
+  OnChanges
 } from '@angular/core';
 
 import { Observable, Subscription } from 'rxjs';
@@ -53,7 +54,7 @@ type UrlOrPoCustomizationFunction = string | (() => PoPageDynamicSearchOptions);
 })
 export class PoPageDynamicSearchComponent
   extends PoPageDynamicSearchBaseComponent
-  implements OnInit, OnDestroy, AfterViewInit
+  implements OnInit, OnDestroy, AfterViewInit, OnChanges
 {
   @ViewChild(PoAdvancedFilterComponent, { static: true }) poAdvancedFilter: PoAdvancedFilterComponent;
   @ViewChild(PoPageListComponent, { static: true }) poPageList: PoPageListComponent;
