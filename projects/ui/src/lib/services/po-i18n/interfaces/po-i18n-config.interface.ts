cf4a38ff047189986d354f173bddfff685450041
Pedro Henrique Pinheiro Domingues_ <domingues.pedro@totvs.com.br>
2025-02-20 16:03:35 -0300

    Revert "fix(i18n): habilita o I18N_CONFIG com multi: true"
    This reverts commit a0ac8edc5e7d38ce83cc84fa8cd922e8e4b5f450.

--- projects/ui/src/lib/services/po-i18n/interfaces/po-i18n-config.interface.ts
+++ projects/ui/src/lib/services/po-i18n/interfaces/po-i18n-config.interface.ts
@@ -1,5 +1,4 @@
 import { PoI18nConfigDefault } from './po-i18n-config-default.interface';
-import { PoI18nConfigContext } from './po-i18n-config-context.interface';
 
 /**
  * @description
@@ -77,5 +76,5 @@ export interface PoI18nConfig {
    * ```
    * > Caso a constante contenha alguma literal que o serviço não possua será utilizado a literal da constante.
    */
-  contexts: PoI18nConfigContext;
+  contexts: object;
 }
