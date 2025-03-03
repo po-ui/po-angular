8cbc3faafa8df72a6e17e66646a3f28b317cc7eb
renanarosario <renan.arosario@totvs.com.br>
2025-03-03 13:51:23 -0300

    feat(i18n): garantir merge correto de literais no PO UI e THF-Components
    Ajustado `getLiterals()` para garantir que as literais sejam exibidas
    corretamente, independente da configuração utilizada. Agora, ao utilizar
    o PO UI e o THF-Components, o serviço i18n realiza o merge das literais
    da aplicação client e das bibliotecas de terceiros, evitando sobreposição
    indevida e garantindo a exibição esperada.
    
    Fixes DTHFUI-10854

--- projects/ui/src/lib/services/po-i18n/po-i18n.service.ts
+++ projects/ui/src/lib/services/po-i18n/po-i18n.service.ts
@@ -1,21 +1,60 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 
 import { PoLanguageService } from './../po-language/po-language.service';
 
 import { PoI18nBaseService } from './po-i18n-base.service';
 import { PoI18nConfig } from './interfaces/po-i18n-config.interface';
+import { I18N_CONFIG } from './po-i18n-config-injection-token';
 
 /**
  * @docsExtends PoI18nBaseService
  */
 
-@Injectable({
-  providedIn: 'root'
-})
-export class PoI18nService extends PoI18nBaseService {}
+@Injectable()
+export class PoI18nService extends PoI18nBaseService {
+  constructor(
+    @Inject(I18N_CONFIG) configs: PoI18nConfig | Array<PoI18nConfig>,
+    http: HttpClient,
+    languageService: PoLanguageService
+  ) {
+    const merged = Array.isArray(configs) ? mergePoI18nConfigs(configs) : configs;
+
+    super(merged, http, languageService);
+  }
+}
 
 // Função usada para retornar instância para o módulo po-i18n.module
-export function returnPoI18nService(config: PoI18nConfig, http: HttpClient, languageService: PoLanguageService) {
-  return new PoI18nService(config, http, languageService);
+export function returnPoI18nService(
+  configs: Array<PoI18nConfig>,
+  http: HttpClient,
+  languageService: PoLanguageService
+) {
+  const mergedObject = mergePoI18nConfigs(configs);
+
+  return new PoI18nService(mergedObject, http, languageService);
+}
+
+export function mergePoI18nConfigs(objects: Array<any>): any {
+  return objects.reduce(
+    (acc, current) => {
+      if (!acc.default) {
+        acc.default = { ...current.default };
+      }
+
+      Object.entries(current.contexts || {}).forEach(([context, languages]) => {
+        acc.contexts[context] = acc.contexts[context] || {};
+
+        Object.entries(languages).forEach(([lang, translations]) => {
+          acc.contexts[context][lang] = {
+            ...acc.contexts[context][lang],
+            ...translations
+          };
+        });
+      });
+
+      return acc;
+    },
+    { contexts: {} }
+  );
 }
