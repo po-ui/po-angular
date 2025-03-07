bea66af5d452759fb0565af5bf69850318813863
CSimoesJr <ju_csimoes@hotmail.com>
2025-03-07 18:40:10 -0300

    feat(chart): implementa Animalia DS no chart tipo linha
    Implementa propriedades para o grafico;
    Implementa testes unitários.
    
    Fixes DTHFUI-11041

---
+++ projects/ui/src/lib/components/po-chart/interfaces/po-chart-literals.interface.ts
@@ -0,0 +1,13 @@
+/**
+ * @usedBy PoChartComponent
+ *
+ * @description
+ *
+ * Interface para definição dos literais usadas no `po-chart`.
+ */
+export interface PoChartLiterals {
+  downloadCSV?: string;
+  exportCSV?: string;
+  exportJPG?: string;
+  exportPNG?: string;
+}
