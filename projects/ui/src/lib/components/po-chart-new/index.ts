b9e700c6415b1746c4f1b8f1c56f606598df24a2
CSimoesJr <ju_csimoes@hotmail.com>
2025-05-06 13:13:44 -0300

    feat(chart): implementa Animalia DS no chart tipo pie

---
+++ projects/ui/src/lib/components/po-chart-new/index.ts
@@ -0,0 +1,6 @@
+export * from './po-chart-new.component';
+
+export * from './po-chart-new.module';
+
+/* Ao apagar esse arquivo, lembre também de remover a linha "export * from './po-chart-new/index'" no arquivo index.css, que fica em:
+po-angular\projects\ui\src\lib\components. */
