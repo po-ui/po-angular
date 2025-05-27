746a9906b2540ff3c053595fb1eda31fca4e797b
Arthur Rodrigues Polidorio <arthur.polidorio@totvs.com.br>
2025-05-27 14:25:47 -0300

    feat(chart): implementa migração do po-chart-new para po-chart

--- projects/ui/src/lib/components/po-chart-new/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './po-chart-new.component';
-
-export * from './po-chart-new.module';
-
-/* @ToDo - Ao apagar esse arquivo, lembre também de remover a linha "export * from './po-chart-new/index'" no arquivo index.css, que fica em:
-po-angular\projects\ui\src\lib\components. */
