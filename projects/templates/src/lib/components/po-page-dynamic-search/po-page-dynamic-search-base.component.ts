acf02ba6534238ed50ff6fb8829450f06a4ef72a
ana.espejo <ana.espejo@totvs.com.br>
2025-06-20 11:11:41 -0300

    feat(page): deprecia tokens

--- projects/templates/src/lib/components/po-page-dynamic-search/po-page-dynamic-search-base.component.ts
+++ projects/templates/src/lib/components/po-page-dynamic-search/po-page-dynamic-search-base.component.ts
@@ -57,6 +57,20 @@ export const poPageDynamicSearchLiteralsDefault = {
  *
  * Componente com as ações de pesquisa já definidas, bastando que o desenvolvedor implemente apenas a chamada para as APIs
  * e exiba as informações.
+ *
+ * #### Tokens customizáveis
+ *
+ * > Para maiores informações, acesse o guia [Personalizando o Tema Padrão com Tokens CSS](https://po-ui.io/guides/theme-customization).
+ *
+ * | Propriedade         | Descrição                                                                        | Valor Padrão                         |                      |
+ * |---------------------|--------------------------------------------------------------------------------- |--------------------------------------|----------------------|
+ * | **Header**          |                                                                                  |                                      |                      |
+ * | `--padding`         | Espaçamento (top `@deprecated 20.x.x`, right, bottom `@deprecated 20.x.x`, left) | `0.5rem (@deprecated 20.x.x) 1.5rem` |                      |
+ * | `--gap`             | Espaçamento entre os breadcrumbs e o título                                      | `1.5rem`                             | `@deprecated 20.x.x` |
+ * | `--gap-actions`     | Espaçamento entre as ações                                                       | `0.5rem`                             |                      |
+ * | `--font-family`     | Família tipográfica do título                                                    | `--font-family-theme`                |                      |
+ * | **Content**         |                                                                                  |                                      |                      |
+ * | `--padding-content` | Espaçamento (top, right, bottom, left)                                           | `0.5rem 1.5rem`                      | `@deprecated 20.x.x` |
  */
 @Directive()
 export abstract class PoPageDynamicSearchBaseComponent {
--- projects/templates/src/lib/components/po-page-job-scheduler/po-page-job-scheduler-base.component.ts
+++ projects/templates/src/lib/components/po-page-job-scheduler/po-page-job-scheduler-base.component.ts
@@ -19,6 +19,20 @@ import { PoPageJobSchedulerService } from './po-page-job-scheduler.service';
  * sem a necessidade de criar componentes e tratamentos dos dados.
  *
  * Veja mais sobre os padrões utilizados nas requisições no [Guia de implementação de APIs](guides/api).
+ *
+ * #### Tokens customizáveis
+ *
+ * > Para maiores informações, acesse o guia [Personalizando o Tema Padrão com Tokens CSS](https://po-ui.io/guides/theme-customization).
+ *
+ * | Propriedade         | Descrição                                                                        | Valor Padrão                         |                      |
+ * |---------------------|--------------------------------------------------------------------------------- |--------------------------------------|----------------------|
+ * | **Header**          |                                                                                  |                                      |                      |
+ * | `--padding`         | Espaçamento (top `@deprecated 20.x.x`, right, bottom `@deprecated 20.x.x`, left) | `0.5rem (@deprecated 20.x.x) 1.5rem` |                      |
+ * | `--gap`             | Espaçamento entre os breadcrumbs e o título                                      | `1.5rem`                             | `@deprecated 20.x.x` |
+ * | `--gap-actions`     | Espaçamento entre as ações                                                       | `0.5rem`                             |                      |
+ * | `--font-family`     | Família tipográfica do título                                                    | `--font-family-theme`                |                      |
+ * | **Content**         |                                                                                  |                                      |                      |
+ * | `--padding-content` | Espaçamento (top, right, bottom, left)                                           | `0.5rem 1.5rem`                      | `@deprecated 20.x.x` |
  */
 @Directive()
 export class PoPageJobSchedulerBaseComponent implements OnDestroy {
