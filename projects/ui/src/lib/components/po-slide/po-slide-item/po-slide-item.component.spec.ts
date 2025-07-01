d6ca20cf1150c6831ecfa449b0b2e8ab80c828a2
Anderson Gregório <anderson.gregorio@totvs.com.br>
2025-07-01 15:47:07 -0300

    feat(angular): deve ser utilizado o Angular v20

--- projects/ui/src/lib/components/po-slide/po-slide-item/po-slide-item.component.spec.ts
+++ projects/ui/src/lib/components/po-slide/po-slide-item/po-slide-item.component.spec.ts
@@ -1,3 +1,4 @@
+import { provideNgReflectAttributes } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
@@ -11,7 +12,8 @@ describe('PoSlideItemComponent:', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule.withRoutes([])],
-      declarations: [PoSlideItemComponent]
+      declarations: [PoSlideItemComponent],
+      providers: [provideNgReflectAttributes()]
     }).compileComponents();
 
     fixture = TestBed.createComponent(PoSlideItemComponent);
