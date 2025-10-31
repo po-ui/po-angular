4a3466b5120961e6a671073cfea75c922a49bd29
arthur.polidorio <arthur.polidorio@totvs.com.br>
2025-10-31 19:32:40 -0300

    feat(page-default): implementa suporte a subníveis no dropdown
    Implementa suporte a utilização de subníveis no dropdown.
    
    Fixes: DTHFUI-12194

--- projects/ui/src/lib/components/po-page/interfaces/po-page-action.interface.ts
+++ projects/ui/src/lib/components/po-page/interfaces/po-page-action.interface.ts
@@ -1,16 +1,17 @@
-import { PoPopupAction } from '../../po-popup/po-popup-action.interface';
+import { PoDropdownAction } from '../../po-dropdown';
 
 /**
  * @description
  * Interface para as ações dos componentes po-page-default e po-page-list.
  *
- * > As propriedades `selected`, `separator` e `type` serão vistas a partir da terceira ação e somente quando
- * definir quatro ações ou mais.
+ * > Quando o array de actions possui quatro ou mais registros, os dois últimos e os seguintes são automaticamente agrupados no po-dropdown.
+ * A partir desse ponto, as propriedades `selected`, `separator`, `type` e `subItems` passam a ter efeito apenas nas ações exibidas dentro do dropdown, ou seja, a partir da terceira ação.
+ * Dessa forma, o uso de subItems (agrupadores dentro do dropdown) só terá efeito quando houver pelo menos quatro ações definidas.
  *
- * @docsExtends PoPopupAction
+ * @docsExtends PoDropdownAction
  *
  * @ignoreExtendedDescription
  *
  * @usedBy PoPageDefaultComponent, PoPageListComponent
  */
-export interface PoPageAction extends PoPopupAction {}
+export interface PoPageAction extends PoDropdownAction {}
