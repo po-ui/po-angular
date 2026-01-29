f0ef643de14410e49f66b905376efd6f2477e4b6
Bruno Pires Brito <bruno.pbrito@totvs.com.br>
2026-01-29 15:01:22 -0300

    feat(calendar): implementa animaliaDS

--- projects/ui/src/lib/components/po-calendar/po-calendar-header/po-calendar-header.component.ts
+++ projects/ui/src/lib/components/po-calendar/po-calendar-header/po-calendar-header.component.ts
@@ -1,4 +1,16 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  TemplateRef,
+  OnChanges,
+  SimpleChanges,
+  inject,
+  ChangeDetectorRef
+} from '@angular/core';
+import { PoComboOption } from '../../po-field/po-combo/interfaces/po-combo-option.interface';
 
 @Component({
   selector: 'po-calendar-header',
@@ -6,14 +18,31 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from 
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: false
 })
-export class PoCalendarHeaderComponent {
+export class PoCalendarHeaderComponent implements OnChanges {
+  private cdr = inject(ChangeDetectorRef);
+
   @Input('p-hide-previous') hidePrevious = false;
-
   @Input('p-hide-next') hideNext = false;
-
+  @Input('p-display-month') displayMonth!: number;
+  @Input('p-display-year') displayYear!: number;
+  @Input('p-display-month-label') displayMonthLabel: string;
+  @Input('p-month-options') monthOptions?: Array<PoComboOption>;
+  @Input('p-header-template') headerTemplate?: TemplateRef<any>;
+  @Input('p-template-context') templateContext: any;
+  @Input('p-size') size: string;
   @Output('p-previous') previous = new EventEmitter<void>();
-
   @Output('p-next') next = new EventEmitter<void>();
+  @Output('p-select-month') selectMonth = new EventEmitter<number>();
+  @Output('p-select-year') selectYear = new EventEmitter<number>();
 
-  constructor() {}
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['templateContext'] || changes['monthOptions'] || changes['headerTemplate']) {
+      this.cdr.markForCheck();
+      try {
+        this.cdr.detectChanges();
+      } catch (e) {
+        // Sileciando erro ExpressionChangedAfterItHasBeenCheckedError
+      }
+    }
+  }
 }
