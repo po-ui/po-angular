451ddb06570d4acd9fcb99f931f2d74764a5518c
alinecbgml <aline.cbgml@gmail.com>
2026-02-24 13:56:12 -0300

    fix(guided-tour): adiciona CommonModule no imports do module

--- projects/ui/src/lib/components/po-guided-tour/po-guided-tour.module.ts
+++ projects/ui/src/lib/components/po-guided-tour/po-guided-tour.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { PoGuidedTourComponent } from './po-guided-tour.component';
 
 @NgModule({
   imports: [
+        CommonModule
   ],
   declarations: [
     PoGuidedTourComponent
