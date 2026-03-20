4cd315b9426cc5429ddfde9238d23bfb726edf72
luiz.vasconcellos <luiz.vasconcellos@totvs.com.br>
2026-03-20 14:28:40 +0000

    feat(timepicker): implementa componente po-timepicker
    Implementa o componente de acordo com as definições do AnimaliaDS.
    
    Fixes DTHFUI-7811

---
+++ projects/ui/src/lib/components/po-field/po-timepicker/po-timepicker.literals.ts
@@ -0,0 +1,46 @@
+export const poTimepickerLiterals = {
+  en: {
+    field: 'Time selection',
+    open: 'Open time picker',
+    clean: 'Clear field',
+    invalidTime: 'Invalid time',
+    outOfRangeTime: 'Time out of range',
+    hour: 'Hour',
+    minute: 'Minute',
+    second: 'Second',
+    selectedTime: 'Selected time'
+  },
+  es: {
+    field: 'Selección de hora',
+    open: 'Abrir selector de hora',
+    clean: 'Limpiar campo',
+    invalidTime: 'Hora no válida',
+    outOfRangeTime: 'Hora fuera del período',
+    hour: 'Hora',
+    minute: 'Minuto',
+    second: 'Segundo',
+    selectedTime: 'Hora seleccionada'
+  },
+  pt: {
+    field: 'Seleção de horário',
+    open: 'Abrir seletor de hora',
+    clean: 'Limpar campo',
+    invalidTime: 'Hora inválida',
+    outOfRangeTime: 'Hora fora do período',
+    hour: 'Hora',
+    minute: 'Minuto',
+    second: 'Segundo',
+    selectedTime: 'Horário selecionado'
+  },
+  ru: {
+    field: 'Выбор времени',
+    open: 'Открыть выбор времени',
+    clean: 'Очистить поле',
+    invalidTime: 'Недопустимое время',
+    outOfRangeTime: 'Время вне допустимого диапазона',
+    hour: 'Час',
+    minute: 'Минута',
+    second: 'Секунда',
+    selectedTime: 'Выбранное время'
+  }
+};
