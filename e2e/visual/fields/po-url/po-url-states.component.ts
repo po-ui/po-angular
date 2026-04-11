80b9274c64eaaed2b03d48e6bf53d4d3149eece7
aline.lariguet <alinecbgmoreira@gmail.com>
2026-04-11 01:47:12 +0000

    feat(visual-regression): testes visuais para todos os fields

---
+++ e2e/visual/fields/po-url/po-url-states.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visual-test-po-url-states',
+  templateUrl: './po-url-states.component.html',
+  standalone: false
+})
+export class VisualTestPoUrlStatesComponent {
+  readonlyValue = 'https://po-ui.io';
+}
