8d1e3a9b9725dd171a97dd1d1190886f2bdcee26
Fabiana Monteiro <fabiana.monteiro@totvs.com.br>
2026-05-06 12:13:14 -0300

    feat(popup): adiciona suporte a subníveis
    Adiciona propriedade subItems na interface PoPopupAction e implementa
    detecção automática de subníveis.
    
    Fixes DTHFUI-12747

--- projects/ui/src/lib/components/po-dropdown/po-dropdown-action.interface.ts
+++ projects/ui/src/lib/components/po-dropdown/po-dropdown-action.interface.ts
@@ -7,17 +7,8 @@ import { PoPopupAction } from '../po-popup/po-popup-action.interface';
  *
  * @docsExtends PoPopupAction
  *
+ * @ignoreExtendedDescription
+ *
  * @usedBy PoDropdownComponent
  */
-export interface PoDropdownAction extends PoPopupAction {
-  /**
-   * @optional
-   *
-   * @description
-   *
-   * Array de ações (`PoDropdownAction`) usado para criar agrupadores de subitens (submenus).
-   *
-   * > Recomenda-se limitar a navegação a, no máximo, três níveis hierárquicos.
-   */
-  subItems?: Array<PoDropdownAction>;
-}
+export interface PoDropdownAction extends PoPopupAction {}
