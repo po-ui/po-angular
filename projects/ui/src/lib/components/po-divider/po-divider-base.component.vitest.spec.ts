a8f96a97d1f15532c5e5278b76373948c447c195
Luiz Gustavo da Silva Vasconcellos <luiz.vasconcellos@totvs.com.br>
2026-07-24 16:29:06 -0300

    test(divider): migrate testes para vitest

---
+++ projects/ui/src/lib/components/po-divider/po-divider-base.component.vitest.spec.ts
@@ -0,0 +1,39 @@
+import { describe, it, expect } from 'vitest';
+
+import { PoDividerBaseComponent } from './po-divider-base.component';
+
+describe('PoDividerComponent:', () => {
+  const component = new PoDividerBaseComponent();
+
+  it('should be created', () => {
+    expect(component instanceof PoDividerBaseComponent).toBeTruthy();
+  });
+
+  it('should set Coordinates to divider small', () => {
+    component.borderWidth = 'small';
+
+    expect(component.coordinateX1).toEqual('0.1%');
+    expect(component.coordinateX2).toEqual('99.9%');
+  });
+
+  it('should set Coordinates to divider small if value invalid', () => {
+    component.borderWidth = 'extra-large';
+
+    expect(component.coordinateX1).toEqual('0.1%');
+    expect(component.coordinateX2).toEqual('99.9%');
+  });
+
+  it('should set Coordinates to divider medium', () => {
+    component.borderWidth = 'medium';
+
+    expect(component.coordinateX1).toEqual('0.2%');
+    expect(component.coordinateX2).toEqual('99.8%');
+  });
+
+  it('should set Coordinates to divider large', () => {
+    component.borderWidth = 'large';
+
+    expect(component.coordinateX1).toEqual('0.3%');
+    expect(component.coordinateX2).toEqual('99.7%');
+  });
+});
---
+++ projects/ui/src/lib/components/po-divider/po-divider.component.vitest.spec.ts
@@ -0,0 +1,46 @@
+import { describe, it, expect, beforeEach } from 'vitest';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PoDividerBaseComponent } from './po-divider-base.component';
+import { PoDividerComponent } from './po-divider.component';
+
+describe('PoDividerComponent:', () => {
+  let component: PoDividerComponent;
+  let fixture: ComponentFixture<PoDividerComponent>;
+
+  let nativeElement: any;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [PoDividerComponent]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PoDividerComponent);
+    component = fixture.componentInstance;
+
+    nativeElement = fixture.debugElement.nativeElement;
+  });
+
+  it('should be created', () => {
+    expect(component instanceof PoDividerBaseComponent).toBeTruthy();
+    expect(component instanceof PoDividerComponent).toBeTruthy();
+  });
+
+  describe('Templates:', () => {
+    it('should add `po-divider-label` if `label` contains value.', () => {
+      component.label = 'PO Divider';
+
+      fixture.detectChanges();
+
+      expect(nativeElement.querySelector('.po-divider-label')).toBeTruthy();
+    });
+
+    it('shouldn`t add `po-divider-label` if `label` doesn`t contain any value.', () => {
+      component.label = null;
+
+      fixture.detectChanges();
+
+      expect(nativeElement.querySelector('.po-divider-label')).toBeFalsy();
+    });
+  });
+});
