ce71d2ac7dd7dbb474df9d8b0cfb477aa5da6842
Luiz Gustavo da Silva Vasconcellos <luiz.vasconcellos@totvs.com.br>
2026-07-27 17:40:17 -0300

    test(label): migra testes para vitest

---
+++ projects/ui/src/lib/components/po-label/po-label.component.vitest.spec.ts
@@ -0,0 +1,48 @@
+import { describe, it, expect, beforeEach } from 'vitest';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { expectPropertiesValues } from '../../util-test/util-expect.spec';
+
+import { PoLabelComponent } from './po-label.component';
+
+describe('PoLabelComponent', () => {
+  let component: PoLabelComponent;
+  let fixture: ComponentFixture<PoLabelComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [PoLabelComponent]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(PoLabelComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  const booleanValidTrueValues = [true, 'true', 1, ''];
+  const booleanValidFalseValues = [false, 'false', 0];
+  const booleanInvalidValues = [undefined, null, 2, 'string'];
+
+  it('should create', () => {
+    expect(component instanceof PoLabelComponent).toBeTruthy();
+  });
+
+  describe('Properties: ', () => {
+    it('should update property `p-disabled` with valid values', () => {
+      expectPropertiesValues(component, 'disabled', booleanValidTrueValues, true);
+      expectPropertiesValues(component, 'disabled', booleanValidFalseValues, false);
+    });
+
+    it('should update property `p-disabled` with `false` when invalid values', () => {
+      expectPropertiesValues(component, 'disabled', booleanInvalidValues, false);
+    });
+
+    it('should update property `p-field` with valid values', () => {
+      expectPropertiesValues(component, 'field', booleanValidTrueValues, true);
+      expectPropertiesValues(component, 'field', booleanValidFalseValues, false);
+    });
+
+    it('should update property `p-field` with `false` when invalid values', () => {
+      expectPropertiesValues(component, 'field', booleanInvalidValues, false);
+    });
+  });
+});
